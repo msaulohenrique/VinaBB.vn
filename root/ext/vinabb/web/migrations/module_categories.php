13403e0a3e1b09e878024e6e6ae131424acfcdc7
Khoa Nguyen <nedkavn@gmail.com>
2016-08-15 17:54:41 +0700

    New module categories, new features

--- root/ext/vinabb/web/migrations/module_categories.php
+++ root/ext/vinabb/web/migrations/module_categories.php
@@ -15,17 +15,28 @@ class module_categories extends migration
 	public function update_data()
 	{
 		return array(
-			// Module categories
+			// Categories
 			array('module.add', array(
 				'acp',
 				'',
 				'ACP_CAT_VINABB'
 			)),
 
+			// Sub-categories
 			array('module.add', array(
 				'acp',
 				'ACP_CAT_VINABB',
 				'ACP_CAT_VINABB_SETTINGS'
+			)),
+			array('module.add', array(
+				'acp',
+				'ACP_CAT_VINABB',
+				'ACP_CAT_VINABB_BB'
+			)),
+			array('module.add', array(
+				'acp',
+				'ACP_CAT_VINABB',
+				'ACP_CAT_VINABB_CMS'
 			)),
 		);
 	}
