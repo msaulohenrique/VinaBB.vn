a67afdf7c0593ae222e3e8d1223a1f3af2a2704c
Khoa Nguyen <nedkavn@gmail.com>
2016-08-16 18:49:58 +0700

    Create shortcut route for user page, e.g. vinabb.vn/user/{username}

---
+++ root/ext/vinabb/web/controller/user.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of the VinaBB.vn package.
+ *
+ * @copyright (c) VinaBB <vinabb.vn>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ */
+
+namespace vinabb\web\controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class user
+{
+	/** @var string */
+	protected $phpbb_root_path;
+
+	/** @var string */
+	protected $php_ext;
+
+	/**
+	* Constructor
+	*
+	* @param string $phpbb_root_path
+	* @param string $php_ext
+	*/
+	public function __construct($phpbb_root_path, $php_ext)
+	{
+		$this->phpbb_root_path = $phpbb_root_path;
+		$this->php_ext = $php_ext;
+	}
+
+	/**
+	* Controller for route /user/{username}
+	*
+	* @param string $username
+	* @return Response A Symfony Response object
+	*/
+	public function info($username)
+	{
+		redirect(append_sid("{$this->phpbb_root_path}memberlist.{$this->php_ext}", "mode=viewprofile&amp;un=$username"));
+	}
+}
