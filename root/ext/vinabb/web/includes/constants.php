f12f89c43689c0d95c2567af44ca98af7fd500af
Khoa Nguyen <nedkavn@gmail.com>
2016-08-16 15:58:06 +0700

    New constants for phpBB Database

--- root/ext/vinabb/web/includes/constants.php
+++ root/ext/vinabb/web/includes/constants.php
@@ -16,4 +16,10 @@ class constants
 	const MAINTENANCE_MODE_ADMIN = 2;
 	const MAINTENANCE_MODE_MOD = 3;
 	const MAINTENANCE_MODE_USER = 4;
+
+	// Resource types
+	const BB_TYPE_EXT = 1;
+	const BB_TYPE_STYLE = 2;
+	const BB_TYPE_LANG = 3;
+	const BB_TYPE_TOOL = 4;
 }
