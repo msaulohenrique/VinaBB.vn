9d808576587873d83b8bf9888969d5d19d051add
Khoa Nguyen <nedkavn@gmail.com>
2016-08-31 17:00:31 +0700

    Add embed pages for forums, topics and posts

---
+++ root/ext/vinabb/web/controller/embed.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * This file is part of the VinaBB.vn package.
+ *
+ * @copyright (c) VinaBB <vinabb.vn>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ */
+
+namespace vinabb\web\controller;
+
+class embed
+{
+	/** @var \phpbb\auth\auth */
+	protected $auth;
+
+	/** @var \phpbb\db\driver\driver_interface */
+	protected $db;
+
+	/** @var \phpbb\template\template */
+	protected $template;
+
+	/** @var \phpbb\request\request */
+	protected $request;
+
+	/** @var \phpbb\controller\helper */
+	protected $helper;
+
+	/** @var string */
+	protected $phpbb_root_path;
+
+	/** @var string */
+	protected $php_ext;
+
+	/**
+	* Constructor
+	*
+	* @param \phpbb\auth\auth $auth
+	* @param \phpbb\db\driver\driver_interface $db
+	* @param \phpbb\template\template $template
+	* @param \phpbb\request\request $request
+	* @param \phpbb\controller\helper $helper
+	* @param string $phpbb_root_path
+	* @param string $php_ext
+	*/
+	public function __construct(\phpbb\auth\auth $auth,
+								\phpbb\db\driver\driver_interface $db,
+								\phpbb\template\template $template,
+								\phpbb\request\request $request,
+								\phpbb\controller\helper $helper,
+								$phpbb_root_path,
+								$php_ext)
+	{
+		$this->auth = $auth;
+		$this->db = $db;
+		$this->template = $template;
+		$this->request = $request;
+		$this->helper = $helper;
+		$this->phpbb_root_path = $phpbb_root_path;
+		$this->php_ext = $php_ext;
+	}
+
+	public function topic($topic_id)
+	{
+		if (!$topic_id)
+		{
+			trigger_error('NO_TOPIC');
+		}
+
+		$sql = 'SELECT topic_first_post_id
+			FROM ' . TOPICS_TABLE . "
+			WHERE topic_first_post_id = $topic_id";
+		$result = $this->db->sql_query($sql);
+		$post_id = (int) $this->db->sql_fetchfield('topic_first_post_id');
+		$this->db->sql_freeresult($result);
+
+		if ($post_id)
+		{
+			redirect($this->helper->route('vinabb_web_embed_post_route', array('post_id' => $post_id)));
+		}
+		else
+		{
+			trigger_error('NO_TOPIC');
+		}
+	}
+
+	/**
+	* Generate simple topic info for embed content
+	*
+	* @param int $topic_id
+	*/
+	public function post($post_id)
+	{
+		if (!$post_id)
+		{
+			trigger_error('NO_POST');
+		}
+
+		$sql = 'SELECT *
+			FROM ' . POSTS_TABLE . "
+			WHERE post_id = $post_id";
+		$result = $this->db->sql_query($sql);
+
+		if ($row = $this->db->sql_fetchrow($result))
+		{
+			$enable_bbcode = ($row['enable_bbcode']) ? OPTION_FLAG_BBCODE : 0;
+			$enable_smilies = ($row['enable_smilies']) ? OPTION_FLAG_SMILIES : 0;
+			$enable_magic_url = ($row['enable_magic_url']) ? OPTION_FLAG_LINKS : 0;
+			$bbcode_options = $enable_bbcode ^ $enable_smilies ^ $enable_magic_url;
+
+			$this->template->assign_vars(array(
+				'POST_SUBJECT'	=> $row['post_subject'],
+				'POST_TEXT'		=> generate_text_for_display($row['post_text'], $row['bbcode_uid'], $row['bbcode_bitfield'], $bbcode_options),
+			));
+		}
+		$this->db->sql_freeresult($result);
+
+		return $this->helper->render('embed_post.html', '');
+	}
+}
