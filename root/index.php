46497ad8ea126168c70ebef5cb94572a77c54daf
Khoa Nguyen <nedkavn@gmail.com>
2016-09-07 23:30:55 +0700

    Redirect to portal as the first gate

--- root/index.php
+++ root/index.php
@@ -18,15 +18,11 @@ include $phpbb_root_path . 'common.' . $phpEx;
 $user->session_begin();
 $auth->acl($user->data);
 
-$hash = $request->variable('hash', '');
-$mark = $request->variable('mark', '');
-$mark_time = $request->variable('mark_time', 0);
-
 /** @var \phpbb\controller\helper $controller_helper */
 $controller_helper = $phpbb_container->get('controller.helper');
 
 $response = new \Symfony\Component\HttpFoundation\RedirectResponse(
-	$controller_helper->route('vinabb_web_board_route', ($mark == 'forums') ? array('board' => 'board', 'hash' => $hash, 'mark' => 'forums', 'mark_time' => $mark_time) : array('board' => 'board')),
+	$controller_helper->route('vinabb_web_portal_route'),
 	301
 );
 $response->send();
