59ec1117abed2e9016802f4a526766fc9098ab0c
Khoa Nguyen <nedkavn@gmail.com>
2016-09-07 22:56:38 +0700

    Add the action 'Mark forms as read' to board index

--- root/index.php
+++ root/index.php
@@ -17,13 +17,16 @@ include $phpbb_root_path . 'common.' . $phpEx;
 // Start session management
 $user->session_begin();
 $auth->acl($user->data);
-$user->setup();
+
+$hash = $request->variable('hash', '');
+$mark = $request->variable('mark', '');
+$mark_time = $request->variable('mark_time', 0);
 
 /** @var \phpbb\controller\helper $controller_helper */
 $controller_helper = $phpbb_container->get('controller.helper');
 
 $response = new \Symfony\Component\HttpFoundation\RedirectResponse(
-	$controller_helper->route('vinabb_web_board_route', array('board' => 'board')),
+	$controller_helper->route('vinabb_web_board_route', ($mark == 'forums') ? array('board' => 'board', 'hash' => $hash, 'mark' => 'forums', 'mark_time' => $mark_time) : array('board' => 'board')),
 	301
 );
 $response->send();
