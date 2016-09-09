4cbd11b989c4de0c53eb2abb3ede6e678a4a6d1e
Khoa Nguyen <nedkavn@gmail.com>
2016-09-09 23:42:44 +0700

    Redirect from viewforum.php with start=

--- root/viewforum.php
+++ root/viewforum.php
@@ -6,6 +6,8 @@
 * @license GNU General Public License, version 2 (GPL-2.0)
 */
 
+use vinabb\web\includes\constants;
+
 /**
 * @ignore
 */
@@ -15,9 +17,11 @@ $phpEx = substr(strrchr(__FILE__, '.'), 1);
 require "{$phpbb_root_path}common.{$phpEx}";
 
 $forum_id = $request->variable('f', 0);
+$start = $request->variable('start', 0);
+$page = floor($start / $config['topics_per_page']) + 1;
 
 $response = new \Symfony\Component\HttpFoundation\RedirectResponse(
-	$phpbb_container->get('controller.helper')->route('vinabb_web_board_forum_route', array('forum_id' => $forum_id)),
+	$phpbb_container->get('controller.helper')->route('vinabb_web_board_forum_route', ($start) ? array('forum_id' => $forum_id, 'page' => constants::REWRITE_URL_PAGE . $page) : array('forum_id' => $forum_id)),
 	301
 );
 $response->send();
