46b50f4fdb370ff8d2823cdbd43416d990357807
Khoa Nguyen <nedkavn@gmail.com>
2016-09-10 09:11:23 +0700

    Redirect old viewonline.php to the new controller

---
+++ root/viewonline.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+/**
+ * @ignore
+ */
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+require "{$phpbb_root_path}common.{$phpEx}";
+
+$mode = $request->variable('mode', '');
+
+$response = new \Symfony\Component\HttpFoundation\RedirectResponse(
+	$phpbb_container->get('controller.helper')->route('vinabb_web_online_route', (!empty($mode)) ? array('mode' => $mode) : array()),
+	301
+);
+$response->send();
