158bddb1c4d0c088dac6f67d5967399556347097
Khoa Nguyen <nedkavn@gmail.com>
2016-10-10 21:52:31 +0700

    Finished: Map contact modal

--- styles/vinabb/theme/js/custom.js
+++ styles/vinabb/theme/js/custom.js
@@ -1,53 +1,53 @@
-/*
- * 
- * Template js
- */
-
 $(document).ready(function () {
-    $("#mapModal").on("shown.bs.modal", function () {
-        var map = new GMaps({
-            el: '#markermap',
-            lat: 10.746078,
-            lng: 106.666883
+	// Google Map on modal
+	$('#mapModal').on('shown.bs.modal', function () {
+		var map = new GMaps({
+			el: '#markermap',
+			lat: $map_lat,
+			lng: $map_lng
+		});
 
-        });
+		map.addMarker({
+			lat: $map_lat,
+			lng: $map_lng
+		});
 
-        map.addMarker({
-            lat: 10.746078,
-            lng: 106.666883,
-        });
+		google.maps.event.trigger(map, 'resize');
+	});
 
-        google.maps.event.trigger(map, "resize");
-    });
+	// iCheck
+	$('.i-checks').iCheck({
+		checkboxClass: 'icheckbox_square-blue',
+		radioClass: 'iradio_square-blue'
+	});
 
-    $('.i-checks').iCheck({
-        checkboxClass: 'icheckbox_square-blue',
-        radioClass: 'iradio_square-blue'
-    });
+	// Left nav scroll
+	$('.nano').nanoScroller();
 
-    //Left nav scroll
-    $(".nano").nanoScroller();
+	// Left menu collapse
+	$('.left-nav-toggle a').on('click', function (event) {
+		event.preventDefault();
+		$('body').toggleClass('nav-toggle');
+	});
 
-    // Left menu collapse
-    $('.left-nav-toggle a').on('click', function (event) {
-        event.preventDefault();
-        $("body").toggleClass("nav-toggle");
-    });
- // Left menu collapse
-    $('.right-sidebar-toggle').on('click', function (event) {
-        event.preventDefault();
-        $("#right-sidebar-toggle").toggleClass("right-sidebar-toggle");
-    });
-//metis menu
-    $("#menu").metisMenu();
-    //slim scroll
-    $('.scrollDiv').slimScroll({
-        color: '#eee',
-        size: '5px',
-        height: '250px',
-        alwaysVisible: false
-    });
-//tooltip popover
- $('[data-toggle="tooltip"]').tooltip();
-  $('[data-toggle="popover"]').popover();
+	// Left menu collapse
+	$('.right-sidebar-toggle').on('click', function (event) {
+		event.preventDefault();
+		$('#right-sidebar-toggle').toggleClass('right-sidebar-toggle');
+	});
+
+	// Menu
+	$('#menu').metisMenu();
+
+	// Slim scroll
+	$('.scrollDiv').slimScroll({
+		color: '#eee',
+		size: '5px',
+		height: '250px',
+		alwaysVisible: false
+	});
+
+	// Tooltip and popover
+	$('[data-toggle="tooltip"]').tooltip();
+	$('[data-toggle="popover"]').popover();
 });
