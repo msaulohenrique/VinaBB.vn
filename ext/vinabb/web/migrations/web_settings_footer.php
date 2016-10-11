1cb26fa88ac1f2ebecfc3ae8f02a30291885a8f1
Khoa Nguyen <nedkavn@gmail.com>
2016-10-11 16:41:00 +0700

    Add migration file for footer data

---
+++ ext/vinabb/web/migrations/web_settings_footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\web\migrations;
+
+use phpbb\db\migration\migration;
+
+class web_settings_footer extends migration
+{
+	public function update_data()
+	{
+		return array(
+			// Footer
+			array('config.add', array('vinabb_web_manager_name', '')),
+			array('config.add', array('vinabb_web_manager_username', '')),
+		);
+	}
+}
