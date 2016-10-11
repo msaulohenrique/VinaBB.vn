1632ee41bd2582b4b9958a3ab098e9989b1c5e56
Khoa Nguyen <nedkavn@gmail.com>
2016-10-11 16:29:11 +0700

    Add birthday modal on index page

--- ext/vinabb/web/controller/portal.php
+++ ext/vinabb/web/controller/portal.php
@@ -13,20 +13,26 @@ class portal
 	/** @var \phpbb\auth\auth */
 	protected $auth;
 
+	/** @var \phpbb\config\config */
+	protected $config;
+
 	/** @var \phpbb\db\driver\driver_interface */
 	protected $db;
+
+	/** @var \phpbb\event\dispatcher_interface */
+	protected $dispatcher;
 
 	/** @var \phpbb\language\language */
 	protected $language;
 
-	/** @var \phpbb\user */
-	protected $user;
+	/** @var \phpbb\request\request */
+	protected $request;
 
 	/** @var \phpbb\template\template */
 	protected $template;
 
-	/** @var \phpbb\request\request */
-	protected $request;
+	/** @var \phpbb\user */
+	protected $user;
 
 	/** @var \phpbb\controller\helper */
 	protected $helper;
@@ -44,11 +50,13 @@ class portal
 	* Constructor
 	*
 	* @param \phpbb\auth\auth $auth
+	* @param \phpbb\config\config $config
 	* @param \phpbb\db\driver\driver_interface $db
-	* @param \phpbb\user $user
+	* @param \phpbb\event\dispatcher_interface $dispatcher
 	* @param \phpbb\language\language $language
-	* @param \phpbb\template\template $template
 	* @param \phpbb\request\request $request
+	* @param \phpbb\template\template $template
+	* @param \phpbb\user $user
 	* @param \phpbb\controller\helper $helper
 	* @param \phpbb\group\helper $group_helper
 	* @param string $phpbb_root_path
@@ -56,11 +64,13 @@ class portal
 	*/
 	public function __construct(
 		\phpbb\auth\auth $auth,
+		\phpbb\config\config $config,
 		\phpbb\db\driver\driver_interface $db,
-		\phpbb\user $user,
+		\phpbb\event\dispatcher_interface $dispatcher,
 		\phpbb\language\language $language,
-		\phpbb\template\template $template,
 		\phpbb\request\request $request,
+		\phpbb\template\template $template,
+		\phpbb\user $user,
 		\phpbb\controller\helper $helper,
 		\phpbb\group\helper $group_helper,
 		$phpbb_root_path,
@@ -68,11 +78,13 @@ class portal
 	)
 	{
 		$this->auth = $auth;
+		$this->config = $config;
 		$this->db = $db;
-		$this->user = $user;
+		$this->dispatcher = $dispatcher;
 		$this->language = $language;
-		$this->template = $template;
 		$this->request = $request;
+		$this->template = $template;
+		$this->user = $user;
 		$this->helper = $helper;
 		$this->group_helper = $group_helper;
 		$this->phpbb_root_path = $phpbb_root_path;
@@ -81,7 +93,7 @@ class portal
 
 	public function index()
 	{
-		// Grab group details for legend display
+		// Group legend for online users
 		$order_legend = ($this->config['legend_sort_groupname']) ? 'group_name' : 'group_legend';
 
 		if ($this->auth->acl_gets('a_group', 'a_groupadd', 'a_groupdel'))
@@ -126,9 +138,85 @@ class portal
 
 		$legend = implode($this->language->lang('COMMA_SEPARATOR'), $legend);
 
+		// Birthday list
+		$birthdays = array();
+
+		if ($this->config['load_birthdays'] && $this->config['allow_birthdays'] && $this->auth->acl_gets('u_viewprofile', 'a_user', 'a_useradd', 'a_userdel'))
+		{
+			$time = $this->user->create_datetime();
+			$now = phpbb_gmgetdate($time->getTimestamp() + $time->getOffset());
+
+			// Display birthdays of 29th february on 28th february in non-leap-years
+			$leap_year_birthdays = '';
+
+			if ($now['mday'] == 28 && $now['mon'] == 2 && !$time->format('L'))
+			{
+				$leap_year_birthdays = " OR u.user_birthday LIKE '" . $this->db->sql_escape(sprintf('%2d-%2d-', 29, 2)) . "%'";
+			}
+			$sql_ary = array(
+				'SELECT' => 'u.user_id, u.username, u.user_colour, u.user_birthday',
+				'FROM' => array(
+					USERS_TABLE => 'u',
+				),
+				'LEFT_JOIN' => array(
+					array(
+						'FROM' => array(BANLIST_TABLE => 'b'),
+						'ON' => 'u.user_id = b.ban_userid',
+					),
+				),
+				'WHERE' => "(b.ban_id IS NULL OR b.ban_exclude = 1)
+					AND (u.user_birthday LIKE '" . $this->db->sql_escape(sprintf('%2d-%2d-', $now['mday'], $now['mon'])) . "%' $leap_year_birthdays)
+					AND u.user_type IN (" . USER_NORMAL . ', ' . USER_FOUNDER . ')',
+			);
+
+			/**
+			* Event to modify the SQL query to get birthdays data
+			*
+			* @event core.index_modify_birthdays_sql
+			* @var	array	now			The assoc array with the 'now' local timestamp data
+			* @var	array	sql_ary		The SQL array to get the birthdays data
+			* @var	object	time		The user related Datetime object
+			* @since 3.1.7-RC1
+			*/
+			$vars = array('now', 'sql_ary', 'time');
+			extract($this->dispatcher->trigger_event('core.index_modify_birthdays_sql', compact($vars)));
+
+			$sql = $this->db->sql_build_query('SELECT', $sql_ary);
+			$result = $this->db->sql_query($sql);
+			$rows = $this->db->sql_fetchrowset($result);
+			$this->db->sql_freeresult($result);
+
+			foreach ($rows as $row)
+			{
+				$birthday_username = get_username_string('full', $row['user_id'], $row['username'], $row['user_colour']);
+				$birthday_year = (int) substr($row['user_birthday'], -4);
+				$birthday_age = ($birthday_year) ? max(0, $now['year'] - $birthday_year) : '';
+				$birthdays[] = array(
+					'USERNAME'	=> $birthday_username,
+					'AGE'		=> $birthday_age,
+				);
+			}
+
+			/**
+			* Event to modify the birthdays list
+			*
+			* @event core.index_modify_birthdays_list
+			* @var	array	birthdays		Array with the users birthdays data
+			* @var	array	rows			Array with the birthdays SQL query result
+			* @since 3.1.7-RC1
+			*/
+			$vars = array('birthdays', 'rows');
+			extract($this->dispatcher->trigger_event('core.index_modify_birthdays_list', compact($vars)));
+
+			$this->template->assign_block_vars_array('birthdays', $birthdays);
+		}
+
 		// Output
 		$this->template->assign_vars(array(
-			'LEGEND'	=> $legend,
+			'LEGEND'				=> $legend,
+			'TOTAL_BIRTHDAY_USERS'	=> sizeof($birthdays),
+
+			'S_DISPLAY_BIRTHDAY_LIST'	=> $this->config['load_birthdays'],
 		));
 	}
 }
