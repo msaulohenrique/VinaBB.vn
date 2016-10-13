cc8ae625924535aac4df0e8c7dc93da85044207b
Khoa Nguyen <nedkavn@gmail.com>
2016-10-13 21:17:15 +0700

    Portal: Migration file for check version URLs

---
+++ ext/vinabb/web/migrations/web_settings_version_check.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\web\migrations;
+
+use phpbb\db\migration\migration;
+
+class web_settings_version_check extends migration
+{
+	public function update_data()
+	{
+		return array(
+			// Check new versions
+			array('config.add', array('vinabb_web_check_phpbb_url', '')),
+			array('config.add', array('vinabb_web_check_phpbb_gc', 0)),
+			array('config.add', array('vinabb_web_check_phpbb_version', '')),
+			array('config.add', array('vinabb_web_check_phpbb_legacy_version', '')),
+			array('config.add', array('vinabb_web_check_php_url', '')),
+			array('config.add', array('vinabb_web_check_php_gc', 0)),
+			array('config.add', array('vinabb_web_check_php_version', '')),
+			array('config.add', array('vinabb_web_check_php_legacy_version', '')),
+		);
+	}
+}
