9c228f77393008088ea6b6704b9b4de525a71b57
Khoa Nguyen <nedkavn@gmail.com>
2016-10-15 15:21:00 +0700

    Portal: Add new ACP module category - ACP_CAT_PORTAL

---
+++ ext/vinabb/web/migrations/v10x/portal_categories.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\web\migrations\v10x;
+
+use phpbb\db\migration\migration;
+
+class portal_categories extends migration
+{
+	static public function depends_on()
+	{
+		return array('\vinabb\web\migrations\v10x\module_categories');
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('module.add', array(
+				'acp',
+				'ACP_CAT_VINABB',
+				'ACP_CAT_PORTAL'
+			)),
+		);
+	}
+}
