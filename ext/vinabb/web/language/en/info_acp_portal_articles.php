51523921cd25f987474d6d9a19d5db25f50f4824
Khoa Nguyen <nedkavn@gmail.com>
2016-10-15 15:20:25 +0700

    Portal: Language keys for managing articles

---
+++ ext/vinabb/web/language/en/info_acp_portal_articles.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+/**
+* All language files should use UTF-8 as their encoding
+* and the files must not contain a BOM.
+*/
+
+$lang = array_merge($lang, array(
+	'ACP_PORTAL_ARTICLES'	=> 'Manage articles',
+
+	'LOG_PORTAL_ARTICLE_ADD'	=> '<strong>Portal: Created new article</strong><br>» %s',
+	'LOG_PORTAL_ARTICLE_DELETE'	=> '<strong>Portal: Deleted article</strong><br>» %s',
+	'LOG_PORTAL_ARTICLE_EDIT'	=> '<strong>Portal: Edited article</strong><br>» %s',
+));
---
+++ ext/vinabb/web/language/vi/info_acp_portal_articles.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+/**
+* All language files should use UTF-8 as their encoding
+* and the files must not contain a BOM.
+*/
+
+$lang = array_merge($lang, array(
+	'ACP_PORTAL_ARTICLES'	=> 'Quản lý bài viết',
+
+	'LOG_PORTAL_ARTICLE_ADD'	=> '<strong>Trang chủ: Đã tạo bài viết mới</strong><br>» %s',
+	'LOG_PORTAL_ARTICLE_DELETE'	=> '<strong>Trang chủ: Đã xóa bài viết</strong><br>» %s',
+	'LOG_PORTAL_ARTICLE_EDIT'	=> '<strong>Trang chủ: Đã sửa bài viết</strong><br>» %s',
+));
