78efb097f0773dcb7596ac4aad86d66483fdb38d
Khoa Nguyen <nedkavn@gmail.com>
2016-10-19 15:05:41 +0700

    Portal: Add ACP module "Manage categories"

---
+++ ext/vinabb/web/acp/portal_categories_module.php
@@ -0,0 +1,290 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\web\acp;
+
+use vinabb\web\includes\constants;
+
+class portal_categories_module
+{
+	/** @var string */
+	public $u_action;
+
+	public function main($id, $mode)
+	{
+		global $phpbb_container;
+
+		$this->auth = $phpbb_container->get('auth');
+		$this->cache = $phpbb_container->get('cache');
+		$this->db = $phpbb_container->get('dbal.conn');
+		$this->ext_helper = $phpbb_container->get('vinabb.web.helper');
+		$this->language = $phpbb_container->get('language');
+		$this->log = $phpbb_container->get('log');
+		$this->pagination= $phpbb_container->get('pagination');
+		$this->request = $phpbb_container->get('request');
+		$this->template = $phpbb_container->get('template');
+		$this->user = $phpbb_container->get('user');
+
+		$this->table_prefix = $phpbb_container->getParameter('core.table_prefix');
+		$this->portal_categories_table = $this->table_prefix . constants::PORTAL_CATEGORIES_TABLE;
+		$this->portal_articles_table = $this->table_prefix . constants::PORTAL_ARTICLES_TABLE;
+
+		$this->tpl_name = 'acp_portal_categories';
+		$this->page_title = $this->language->lang('ACP_PORTAL_CATS');
+		$this->language->add_lang('acp_portal', 'vinabb/web');
+
+		$action = $this->request->variable('action', '');
+		$action = $this->request->is_set_post('add') ? 'add' : ($this->request->is_set_post('save') ? 'save' : $action);
+
+		// Pagination
+		$start = $this->request->variable('start', 0);
+		$per_page = constants::PORTAL_CATS_PER_PAGE;
+
+		add_form_key('vinabb/web');
+
+		$s_hidden_fields = '';
+		$errors = array();
+
+		switch ($action)
+		{
+			case 'edit':
+				$cat_id = $this->request->variable('id', 0);
+
+				if (!$cat_id)
+				{
+					trigger_error($this->language->lang('NO_PORTAL_CAT_ID') . adm_back_link($this->u_action), E_USER_WARNING);
+				}
+
+				$sql = 'SELECT *
+					FROM ' . $this->portal_categories_table . "
+					WHERE cat_id = $cat_id";
+				$result = $this->db->sql_query($sql);
+				$cat_data = $this->db->sql_fetchrow($result);
+				$this->db->sql_freeresult($result);
+
+				$s_hidden_fields .= '<input type="hidden" name="id" value="' . $cat_id . '" />';
+			// No break
+
+			case 'add':
+				$this->template->assign_vars(array(
+					'CAT_NAME'		=> isset($cat_data['cat_name']) ? $cat_data['cat_name'] : '',
+					'CAT_NAME_VI'	=> isset($cat_data['cat_name_vi']) ? $cat_data['cat_name_vi'] : '',
+					'CAT_VARNAME'	=> isset($cat_data['cat_varname']) ? $cat_data['cat_varname'] : '',
+
+					'U_ACTION'	=> $this->u_action,
+					'U_BACK'	=> $this->u_action,
+
+					'S_EDIT'			=> true,
+					'S_HIDDEN_FIELDS'	=> $s_hidden_fields,
+				));
+
+				return;
+			break;
+
+			case 'save':
+				if (!check_form_key('vinabb/web'))
+				{
+					$errors[] = $this->language->lang('FORM_INVALID');
+				}
+
+				$cat_id = $this->request->variable('id', 0);
+				$cat_name = $this->request->variable('cat_name', '', true);
+				$cat_name_vi = $this->request->variable('cat_name_vi', '', true);
+				$cat_varname = strtolower($this->request->variable('cat_varname', ''));
+
+				if (empty($cat_name) || empty($cat_name_vi))
+				{
+					$errors[] = $this->language->lang('ERROR_PORTAL_CAT_NAME_EMPTY');
+				}
+
+				if (empty($cat_varname))
+				{
+					$errors[] = $this->language->lang('ERROR_PORTAL_CAT_VARNAME_EMPTY');
+				}
+				else
+				{
+					$sql = 'SELECT *
+						FROM ' . $this->portal_categories_table . "
+						WHERE cat_varname = '" . $this->db->sql_escape($cat_varname) . "'";
+					$result = $this->db->sql_query($sql);
+					$rows = $this->db->sql_fetchrowset($result);
+					$this->db->sql_freeresult($result);
+
+					if (sizeof($rows))
+					{
+						$errors[] = $this->language->lang('ERROR_PORTAL_CAT_VARNAME_DUPLICATE', $cat_varname);
+					}
+				}
+
+				if (sizeof($errors))
+				{
+					trigger_error(implode('<br>', $errors) . adm_back_link($this->u_action), E_USER_WARNING);
+				}
+
+				$sql_ary = array(
+					'cat_name'		=> $cat_name,
+					'cat_name_vi'	=> $cat_name_vi,
+					'cat_varname'	=> $cat_varname,
+				);
+
+				if ($cat_id)
+				{
+					$this->db->sql_query('UPDATE ' . $this->portal_categories_table . ' SET ' . $this->db->sql_build_array('UPDATE', $sql_ary) . ' WHERE cat_id = ' . $cat_id);
+				}
+				else
+				{
+					$this->db->sql_query('INSERT INTO ' . $this->portal_categories_table . ' ' . $this->db->sql_build_array('INSERT', $sql_ary));
+				}
+
+				$this->cache->clear_portal_cat_data($mode);
+
+				$log_action = ($cat_id) ? 'LOG_PORTAL_CAT_EDIT' : 'LOG_PORTAL_CAT_ADD';
+				$this->log->add('admin', $this->user->data['user_id'], $this->user->ip, $log_action, false, array($cat_name));
+
+				$message = ($cat_id) ? $this->language->lang('MESSAGE_PORTAL_CAT_EDIT') : $this->language->lang('MESSAGE_BB_CAT_ADD');
+				trigger_error($message . adm_back_link($this->u_action));
+			break;
+
+			case 'delete':
+				$cat_id = $this->request->variable('id', 0);
+
+				if (!$cat_id)
+				{
+					trigger_error($this->language->lang('NO_PORTAL_CAT_ID') . adm_back_link($this->u_action), E_USER_WARNING);
+				}
+
+				if (confirm_box(true))
+				{
+					$sql = 'SELECT COUNT(article_id) AS article_count
+						FROM ' . $this->portal_articles_table . "
+						WHERE cat_id = $cat_id";
+					$result = $this->db->sql_query($sql);
+					$article_count = $this->db->sql_fetchfield('article_count');
+					$this->db->sql_freeresult($result);
+
+					if ($article_count)
+					{
+						trigger_error($this->language->lang('ERROR_PORTAL_CAT_DELETE') . adm_back_link($this->u_action), E_USER_WARNING);
+					}
+
+					$sql = 'SELECT cat_name
+						FROM ' . $this->portal_categories_table . "
+						WHERE cat_id = $cat_id";
+					$result = $this->db->sql_query($sql);
+					$cat_name = $this->db->sql_fetchfield('cat_name');
+					$this->db->sql_freeresult($result);
+
+					$sql = 'DELETE FROM ' . $this->portal_categories_table . "
+						WHERE cat_id = $cat_id";
+					$this->db->sql_query($sql);
+
+					$this->cache->clear_portal_cat_data($mode);
+
+					$this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'LOG_PORTAL_CAT_DELETE', false, array($cat_name));
+
+					trigger_error($this->language->lang('MESSAGE_PORTAL_CAT_DELETE') . adm_back_link($this->u_action));
+				}
+				else
+				{
+					confirm_box(false, $this->language->lang('CONFIRM_PORTAL_CAT_DELETE'), build_hidden_fields(array(
+						'i'			=> $id,
+						'mode'		=> $mode,
+						'id'		=> $cat_id,
+						'action'	=> 'delete',
+					)));
+				}
+			break;
+		}
+
+		// Item counter
+		$sql = 'SELECT cat_id, COUNT(article_id) AS article_count
+			FROM ' . $this->portal_articles_table . '
+			GROUP BY cat_id';
+		$result = $this->db->sql_query($sql);
+
+		$article_count = array();
+		while ($row = $this->db->sql_fetchrow($result))
+		{
+			$article_count[$row['cat_id']] = $row['article_count'];
+		}
+		$this->db->sql_freeresult($result);
+
+		// Manage categories
+		$cats = array();
+		$cat_count = 0;
+		$start = $this->list_portal_cats($this->portal_categories_table, $cats, $cat_count, $per_page, $start);
+
+		foreach ($cats as $row)
+		{
+			$this->template->assign_block_vars('cats', array(
+				'NAME'		=> $row['cat_name'],
+				'NAME_VI'	=> $row['cat_name_vi'],
+				'VARNAME'	=> $row['cat_varname'],
+				'ARTICLES'	=> isset($article_count[$row['cat_id']]) ? $article_count[$row['cat_id']] : 0,
+
+				'U_EDIT'	=> $this->u_action . '&action=edit&id=' . $row['cat_id'],
+				'U_DELETE'	=> $this->u_action . '&action=delete&id=' . $row['cat_id'],
+			));
+		}
+
+		$this->pagination->generate_template_pagination($this->u_action, 'pagination', 'start', $cat_count, $per_page, $start);
+
+		// Output
+		$this->template->assign_vars(array(
+			'TOTAL_CATS'	=> $cat_count,
+
+			'U_ACTION'	=> $this->u_action . "&action=$action&start=$start",
+
+			'S_HIDDEN_FIELDS'	=> $s_hidden_fields,
+		));
+	}
+
+	/**
+	* List categories with pagination
+	*
+	* @param     $portal_categories_table
+	* @param     $mode
+	* @param     $cats
+	* @param     $cat_count
+	* @param int $limit
+	* @param int $offset
+	*
+	* @return int
+	*/
+	private function list_portal_cats(&$portal_categories_table, &$cats, &$cat_count, $limit = 0, $offset = 0)
+	{
+		$sql = "SELECT COUNT(cat_id) AS cat_count
+			FROM $portal_categories_table";
+		$result = $this->db->sql_query($sql);
+		$cat_count = (int) $this->db->sql_fetchfield('cat_count');
+		$this->db->sql_freeresult($result);
+
+		if ($cat_count == 0)
+		{
+			return 0;
+		}
+
+		if ($offset >= $cat_count)
+		{
+			$offset = ($offset - $limit < 0) ? 0 : $offset - $limit;
+		}
+
+		$sql = "SELECT *
+			FROM $portal_categories_table
+			ORDER BY cat_name";
+		$result = $this->db->sql_query_limit($sql, $limit, $offset);
+
+		while ($row = $this->db->sql_fetchrow($result))
+		{
+			$cats[] = $row;
+		}
+		$this->db->sql_freeresult($result);
+
+		return $offset;
+	}
+}
