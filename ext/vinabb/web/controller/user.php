f60d0600c9cbe9dd364135be46c28898e5d0a6b6
Khoa Nguyen <nedkavn@gmail.com>
2016-10-21 23:34:59 +0700

    Prepare for memberlist moving

--- ext/vinabb/web/controller/user.php
+++ ext/vinabb/web/controller/user.php
@@ -10,8 +10,38 @@ namespace vinabb\web\controller;
 
 class user
 {
+	/** @var \phpbb\auth\auth */
+	protected $auth;
+
+	/** @var \phpbb\config\config */
+	protected $config;
+
+	/** @var \phpbb\db\driver\driver_interface */
+	protected $db;
+
+	/** @var \phpbb\event\dispatcher_interface */
+	protected $dispatcher;
+
+	/** @var \phpbb\language\language */
+	protected $language;
+
+	/** @var \phpbb\request\request */
+	protected $request;
+
+	/** @var \phpbb\template\template */
+	protected $template;
+
+	/** @var \phpbb\user */
+	protected $user;
+
+	/** @var \phpbb\controller\helper */
+	protected $helper;
+
+	/** @var \phpbb\group\helper */
+	protected $group_helper;
+
 	/** @var string */
-	protected $phpbb_root_path;
+	protected $root_path;
 
 	/** @var string */
 	protected $php_ext;
@@ -19,13 +49,55 @@ class user
 	/**
 	* Constructor
 	*
-	* @param string $phpbb_root_path
+	* @param \phpbb\auth\auth $auth
+	* @param \phpbb\config\config $config
+	* @param \phpbb\db\driver\driver_interface $db
+	* @param \phpbb\event\dispatcher_interface $dispatcher
+	* @param \phpbb\language\language $language
+	* @param \phpbb\request\request $request
+	* @param \phpbb\template\template $template
+	* @param \phpbb\user $user
+	* @param \phpbb\controller\helper $helper
+	* @param \phpbb\group\helper $group_helper
+	* @param string $root_path
 	* @param string $php_ext
 	*/
-	public function __construct($phpbb_root_path, $php_ext)
+	public function __construct(
+		\phpbb\auth\auth $auth,
+		\phpbb\config\config $config,
+		\phpbb\db\driver\driver_interface $db,
+		\phpbb\event\dispatcher_interface $dispatcher,
+		\phpbb\language\language $language,
+		\phpbb\request\request $request,
+		\phpbb\template\template $template,
+		\phpbb\user $user,
+		\phpbb\controller\helper $helper,
+		\phpbb\group\helper $group_helper,
+		$root_path,
+		$php_ext
+	)
 	{
-		$this->phpbb_root_path = $phpbb_root_path;
+		$this->auth = $auth;
+		$this->config = $config;
+		$this->db = $db;
+		$this->dispatcher = $dispatcher;
+		$this->language = $language;
+		$this->request = $request;
+		$this->template = $template;
+		$this->user = $user;
+		$this->helper = $helper;
+		$this->group_helper = $group_helper;
+		$this->root_path = $root_path;
 		$this->php_ext = $php_ext;
+
+		// Common functions
+		include "{$this->root_path}includes/functions_display.{$this->php_ext}";
+
+		// Common language files
+		$this->language->add_lang(array('memberlist', 'groups'));
+
+		// Setting a variable to let the style designer know where he is...
+		$this->template->assign_var('S_IN_MEMBERLIST', true);
 	}
 
 	public function memberlist()
@@ -40,7 +112,8 @@ class user
 
 	public function contact_admin()
 	{
-
+		define('SKIP_CHECK_BAN', true);
+		define('SKIP_CHECK_DISABLED', true);
 	}
 
 	public function email()
@@ -70,6 +143,9 @@ class user
 
 	public function livesearch()
 	{
-		
+		if (!$this->config['allow_live_searches'])
+		{
+			trigger_error('LIVE_SEARCHES_NOT_ALLOWED');
+		}
 	}
 }
