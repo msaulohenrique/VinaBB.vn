6484846191eca53b50b21cbf2de81df7777b0612
Khoa Nguyen <nedkavn@gmail.com>
2016-10-23 23:54:05 +0700

    Rip off mcp.php

---
+++ mcp.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+/**
+* @ignore
+*/
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+require "{$phpbb_root_path}common.{$phpEx}";
+
+$id = $request->variable('i', '');
+$mode = $request->variable('mode', '');
+
+$response = new \Symfony\Component\HttpFoundation\RedirectResponse(
+	($phpbb_extension_manager->is_enabled('vinabb/web')) ? $phpbb_container->get('controller.helper')->route('vinabb_web_mcp_route', array('id' => $id, 'mode' => $mode)) : "{$phpbb_root_path}index.{$phpEx}",
+	301
+);
+$response->send();
