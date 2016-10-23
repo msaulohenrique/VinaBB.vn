88d27f26e9bf00478d86598104c9d0363d30b68f
Khoa Nguyen <nedkavn@gmail.com>
2016-10-23 23:13:52 +0700

    Prepare some services for MCP controller

--- ext/vinabb/web/controller/mcp.php
+++ ext/vinabb/web/controller/mcp.php
@@ -10,8 +10,41 @@ namespace vinabb\web\controller;
 
 class mcp
 {
+	/** @var \phpbb\auth\auth */
+	protected $auth;
+
+	/** @var \phpbb\config\config */
+	protected $config;
+
+	/** @var \phpbb\db\driver\driver_interface */
+	protected $db;
+
+	/** @var \phpbb\event\dispatcher_interface */
+	protected $dispatcher;
+
+	/** @var \phpbb\extension\manager */
+	protected $ext_manager;
+
+	/** @var \phpbb\language\language */
+	protected $language;
+
+	/** @var \phpbb\log\log */
+	protected $log;
+
+	/** @var \phpbb\request\request */
+	protected $request;
+
+	/** @var \phpbb\template\template */
+	protected $template;
+
+	/** @var \phpbb\user */
+	protected $user;
+
+	/** @var \phpbb\controller\helper */
+	protected $helper;
+
 	/** @var string */
-	protected $phpbb_root_path;
+	protected $root_path;
 
 	/** @var string */
 	protected $php_ext;
@@ -19,13 +52,51 @@ class mcp
 	/**
 	* Constructor
 	*
-	* @param string $phpbb_root_path
+	* @param \phpbb\auth\auth $auth
+	* @param \phpbb\config\config $config
+	* @param \phpbb\db\driver\driver_interface $db
+	* @param \phpbb\event\dispatcher_interface $dispatcher
+	* @param \phpbb\extension\manager $ext_manager
+	* @param \phpbb\language\language $language
+	* @param \phpbb\log\log $log
+	* @param \phpbb\request\request $request
+	* @param \phpbb\template\template $template
+	* @param \phpbb\user $user
+	* @param \phpbb\controller\helper $helper
+	* @param string $root_path
 	* @param string $php_ext
 	*/
-	public function __construct($phpbb_root_path, $php_ext)
+	public function __construct(
+		\phpbb\auth\auth $auth,
+		\phpbb\config\config $config,
+		\phpbb\db\driver\driver_interface $db,
+		\phpbb\event\dispatcher_interface $dispatcher,
+		\phpbb\extension\manager $ext_manager,
+		\phpbb\language\language $language,
+		\phpbb\log\log $log,
+		\phpbb\request\request $request,
+		\phpbb\template\template $template,
+		\phpbb\user $user,
+		\phpbb\controller\helper $helper,
+		$root_path,
+		$php_ext
+	)
 	{
-		$this->phpbb_root_path = $phpbb_root_path;
+		$this->auth = $auth;
+		$this->config = $config;
+		$this->db = $db;
+		$this->dispatcher = $dispatcher;
+		$this->ext_manager = $ext_manager;
+		$this->language = $language;
+		$this->log = $log;
+		$this->request = $request;
+		$this->template = $template;
+		$this->user = $user;
+		$this->helper = $helper;
+		$this->root_path = $root_path;
 		$this->php_ext = $php_ext;
+
+		$this->ext_root_path = $this->ext_manager->get_extension_path('vinabb/web', true);
 	}
 
 	public function main()
