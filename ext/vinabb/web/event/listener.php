569e5170817be4a6422291aab0339339a51baec7
Khoa Nguyen <nedkavn@gmail.com>
2016-10-28 15:03:30 +0700

    Updating routing.yml with {slash}

--- ext/vinabb/web/event/listener.php
+++ ext/vinabb/web/event/listener.php
@@ -304,7 +304,7 @@ class listener implements EventSubscriberInterface
 			'S_FOUNDER'		=> $this->user->data['user_type'] == USER_FOUNDER,
 			'S_VIETNAMESE'	=> $this->user->lang_name == constants::LANG_VIETNAMESE,
 
-			'U_BOARD'			=> $this->helper->route('vinabb_web_board_route', array('board' => 'board')),
+			'U_BOARD'			=> $this->helper->route('vinabb_web_board_route'),
 			'U_MCP'				=> ($this->auth->acl_get('m_') || $this->auth->acl_getf_global('m_')) ? append_sid("{$this->root_path}mcp.{$this->php_ext}", 'i=main&mode=front', true, $this->user->session_id) : '',
 			'U_LANG'			=> ($this->user->data['user_id'] == ANONYMOUS && $this->config['vinabb_web_lang_enable']) ? append_sid("{$this->root_path}index.{$this->php_ext}", "language=$lang_switch") : '',
 			'U_CONTACT_PM'		=> ($this->config['allow_privmsg'] && $this->auth->acl_get('u_sendpm') && $this->config['vinabb_web_manager_user_id']) ? $this->helper->route('vinabb_web_ucp_route', array('id' => 'pm', 'mode' => 'compose', 'u' => $this->config['vinabb_web_manager_user_id'])) : '',
