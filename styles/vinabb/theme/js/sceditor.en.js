b280324545b063b5f5194c3eb72f2dcc2f641ccd
Khoa Nguyen <nedkavn@gmail.com>
2016-11-03 15:08:04 +0700

    SCEditor: Add language JS files

---
+++ styles/vinabb/theme/js/sceditor.en.js
@@ -0,0 +1,43 @@
+(function ($) {
+	'use strict';
+
+	$.sceditor.locale['xx'] = {
+		'Bold': '',
+		'Italic': '',
+		'Underline': '',
+		'Strikethrough': '',
+		'Subscript': '',
+		'Superscript': '',
+		'Align left': '',
+		'Center': '',
+		'Align right': '',
+		'Justify': '',
+		'Font Size': 'Font size',
+		'Font Color': 'Color',
+		'Remove Formatting': 'Clear formatting',
+		'Bullet list': 'List',
+		'Numbered list': 'Ordered list',
+		'Rows:': 'Rows:',
+		'Cols:': 'Columns:',
+		'Insert a table': 'Table',
+		'Code': 'Code',
+		'Width (optional):': 'Width (Optional):',
+		'Height (optional):': 'Height (Optional):',
+		'Insert an image': 'Image',
+		'E-mail:': 'Email address',
+		'Insert an email': 'Email',
+		'URL:': '',
+		'Insert a link': 'Link',
+		'Unlink': 'Remove link',
+		'More': '',
+		'Insert an emoticon': 'Smilies',
+		'Insert': '',
+		'Print': '',
+		'View source': 'BBCode mode',
+		'Description (optional):': 'Text (Optional):',
+		'Insert a Quote': 'Quote',
+		'Maximize': '',
+
+		dateFormat: 'day-month-year'
+	};
+})(jQuery);
---
+++ styles/vinabb/theme/js/sceditor.vi.js
@@ -0,0 +1,43 @@
+(function ($) {
+	'use strict';
+
+	$.sceditor.locale['vi'] = {
+		'Bold': 'In đậm',
+		'Italic': 'In nghiêng',
+		'Underline': 'Gạch dưới',
+		'Strikethrough': 'Gạch bỏ',
+		'Subscript': 'Chỉ số dưới',
+		'Superscript': 'Chỉ số trên',
+		'Align left': 'Canh trái',
+		'Center': 'Canh giữa',
+		'Align right': 'Canh phải',
+		'Justify': 'Canh đều',
+		'Font Size': 'Cỡ chữ',
+		'Font Color': 'Màu chữ',
+		'Remove Formatting': 'Xóa định dạng',
+		'Bullet list': 'Danh sách',
+		'Numbered list': 'Danh sách thứ tự',
+		'Rows:': 'Số dòng:',
+		'Cols:': 'Số cột:',
+		'Insert a table': 'Bảng',
+		'Code': 'Chèn mã',
+		'Width (optional):': 'Chiều rộng (Tùy chọn):',
+		'Height (optional):': 'Chiều cao (Tùy chọn):',
+		'Insert an image': 'Hình',
+		'E-mail:': 'Địa chỉ hộp thư',
+		'Insert an email': 'Hộp thư',
+		'URL:': 'Liên kết:',
+		'Insert a link': 'Liên kết',
+		'Unlink': 'Xóa liên kết',
+		'More': 'Xem thêm',
+		'Insert an emoticon': 'Biểu tượng vui',
+		'Insert': 'Chèn',
+		'Print': 'In',
+		'View source': 'Chế độ BBCode',
+		'Description (optional):': 'Tên liên kết (Tùy chọn):',
+		'Insert a Quote': 'Trích dẫn',
+		'Maximize': 'Phóng to',
+
+		dateFormat: 'day-month-year'
+	};
+})(jQuery);
