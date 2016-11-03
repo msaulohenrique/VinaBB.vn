e6e626289b5a67c86d44f5a765421abaa3d3d1fc
Khoa Nguyen <nedkavn@gmail.com>
2016-11-03 11:48:36 +0700

    SCEditor: Custom BBCode commands

--- styles/vinabb/theme/js/custom.js
+++ styles/vinabb/theme/js/custom.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
 			plugins: 'bbcode',
 			enablePasteFiltering: true,
 			toolbar: 'bold,italic,underline,strike,superscript,subscript|size,color,removeformat|left,center,right,justify|bulletlist,orderedlist,table|quote,code,emoticon|image,link,unlink|source,maximize',
-			style: './styles/vinabb/theme/css/jquery.sceditor.min.css',
+			style: $t_theme_path + '/css/jquery.sceditor.min.css',
 			emoticonsRoot: "",
 			emoticons: {
 				dropdown: {
@@ -45,7 +45,7 @@ $(document).ready(function () {
 					":d": './images/smilies/1.png',
 					":f": './images/smilies/1.png',
 					":g": './images/smilies/1.png',
-					":d": './images/smilies/1.png',
+					":x": './images/smilies/1.png',
 					":r": './images/smilies/1.png',
 					":e": './images/smilies/1.png',
 				}
@@ -55,6 +55,161 @@ $(document).ready(function () {
 		});
 
 		$('a[data-sceditor-command]').attr('data-toggle', 'tooltip').attr('data-placement', 'bottom');
+
+		$.sceditor.plugins.bbcode.bbcode
+			.set("list", {
+				html: function(element, attrs, content) {
+					var type = (attrs.defaultattr === '1' ? 'ol' : 'ul');
+
+					return '<' + type + '>' + content + '</' + type + '>';
+				},
+				breakAfter: false
+			})
+
+			.set("ul", { format: function($elm, content) { return '[list]' + content +'[/list]'; }})
+			.set("ol", { format: function($elm, content) { return '[list=1]' + content +'[/list]'; }})
+			.set("li", { format: function($elm, content) { return '[*]' + content; }})
+			.set("li", { format: function($elm, content) { return '[*]' + content; }})
+			.set("*", { excludeClosing: true, isInline: false });
+
+		$.sceditor.command
+			.set("bulletlist", { txtExec: ["[list]\n[*]", "\n[/list]"] })
+			.set("orderedlist", { txtExec: ["[list=1]\n[*]", "\n[/list]"] });
+
+
+		var sizes = ['65', '85', '100', '120', '150', '175', '200'];
+
+		$.sceditor.plugins.bbcode.bbcode.set('size', {
+			format: function ($elem, content) {
+				var fontSize,
+					sizesIdx = 0,
+					size = $elem.data('scefontsize');
+
+				if (!size) {
+					fontSize = $elem.css('fontSize');
+
+					// Most browsers return px value but IE returns 1-7
+					if (fontSize.indexOf('px') > -1) {
+						// convert size to an int
+						fontSize = ~~(fontSize.replace('px', ''));
+
+						if (fontSize > 31) {
+							sizesIdx = 6;
+						}
+						else if (fontSize > 23) {
+							sizesIdx = 5;
+						}
+						else if (fontSize > 17) {
+							sizesIdx = 4;
+						}
+						else if (fontSize > 15) {
+							sizesIdx = 3;
+						}
+						else if (fontSize > 12) {
+							sizesIdx = 2;
+						}
+						else if (fontSize > 9) {
+							sizesIdx = 1;
+						}
+					}
+					else {
+						sizesIdx = ~~fontSize;
+					}
+
+					if (sizesIdx > 6) {
+						sizesIdx = 6;
+					}
+					else if (sizesIdx < 0) {
+						sizesIdx = 0;
+					}
+
+					size = sizes[sizesIdx];
+				}
+
+				return '[size=' + size + ']' + content + '[/size]';
+			},
+			html: function (token, attrs, content) {
+				return '<span data-scefontsize="' + attrs.defaultattr + '" style="font-size:' + attrs.defaultattr + '%">' + content + '</span>';
+			}
+		});
+
+		$.sceditor.command.set('size', {
+			_dropDown: function (editor, caller, callback) {
+				var content = $('<div />'),
+					clickFunc = function (e) {
+						callback($(this).data('size'));
+						editor.closeDropDown(true);
+						e.preventDefault();
+					},
+					size;
+
+				for (var i = 1; i < 7; i++) {
+					// Only consider maxsize when set greater 0
+					content.append($('<a class="sceditor-fontsize-option" data-size="' + i + '" href="#"><font size="' + i + '">' + i + '</font></a>').click(clickFunc));
+				}
+
+				editor.createDropDown(caller, 'fontsize-picker', content);
+			},
+			txtExec: function (caller) {
+				var editor = this;
+
+				$.sceditor.command.get('size')._dropDown(
+					editor,
+					caller,
+					function (sizesIdx) {
+						sizesIdx = ~~sizesIdx;
+						if (sizesIdx > 6) {
+							sizesIdx = 6;
+						}
+						else if (sizesIdx < 0) {
+							sizesIdx = 0;
+						}
+
+						editor.insertText('[size=' + sizes[sizesIdx] + ']', '[/size]');
+					}
+				);
+			}
+		});
+
+		$.sceditor.plugins.bbcode.bbcode.set('quote', {
+			format: function (element, content) {
+				var author = '',
+					$element = $(element),
+					$cite = $element.children('cite').first();
+
+				if (1 === $cite.length || $element.data('author')) {
+					author = $element.data('author') || $cite.text().replace(/(^\s+|\s+$)/g, '').replace(/:$/, '');
+
+					$element.data('author', author);
+					$cite.remove();
+
+					content = this.elementToBbcode($element);
+					author = '=' + author;
+
+					$element.prepend($cite);
+				}
+
+				return '[quote' + author + ']' + content + '[/quote]';
+			},
+			html: function (token, attrs, content) {
+				var addition = '';
+
+				if ("undefined" !== typeof attrs.defaultattr) {
+					content = '<cite>' + attrs.defaultattr + ':</cite>' + content;
+					addition = ' data-author="' + attrs.defaultattr + '"';
+				}
+				else {
+					addition = ' class="uncited"'
+				}
+
+				return '<blockquote' + addition + '>' + content + '</blockquote>';
+			},
+			quoteType: function (val, name) {
+				return '"' + val.replace('"', '\\"') + '"';
+			},
+			breakStart: false,
+			breakEnd: false
+		});
 	}
 
 	// iCheck
