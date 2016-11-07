571e608801d6727692b5b182d48631648bfc9734
Khoa Nguyen <nedkavn@gmail.com>
2016-11-07 19:56:29 +0700

    Portal: Update schema for nested-categories

--- ext/vinabb/web/migrations/v10x/portal_schema.php
+++ ext/vinabb/web/migrations/v10x/portal_schema.php
@@ -10,76 +10,91 @@ namespace vinabb\web\migrations\v10x;
 
 use phpbb\db\migration\migration;
 
+/**
+* Database schema for portal
+*/
 class portal_schema extends migration
 {
+	/**
+	* Update schema
+	*
+	* @return array
+	*/
 	public function update_schema()
 	{
-		return array(
-			'add_tables' => array(
-				$this->table_prefix . 'portal_categories' => array(
-					'COLUMNS' => array(
-						'cat_id'		=> array('UINT', null, 'auto_increment'),
-						'cat_name'		=> array('VCHAR_UNI', ''),
-						'cat_name_vi'	=> array('VCHAR_UNI', ''),
-						'cat_varname'	=> array('VCHAR', ''),
-						'cat_icon'		=> array('VCHAR', ''),
-						'cat_order'		=> array('USINT', 0),
-					),
-					'PRIMARY_KEY' => 'cat_id',
-				),
-				$this->table_prefix . 'portal_articles' => array(
-					'COLUMNS' => array(
-						'article_id'			=> array('UINT', null, 'auto_increment'),
-						'cat_id'				=> array('UINT', 0),
-						'article_name'			=> array('VCHAR_UNI', ''),
-						'article_name_seo'		=> array('VCHAR', ''),
-						'article_lang'			=> array('VCHAR:30', ''),
-						'article_img'			=> array('VCHAR', ''),
-						'article_desc'			=> array('TEXT_UNI', ''),
-						'article_text'			=> array('MTEXT_UNI', ''),
-						'article_text_uid'		=> array('VCHAR:8', ''),
-						'article_text_bitfield'	=> array('VCHAR:255', ''),
-						'article_text_options'	=> array('UINT:11', 0),
-						'article_enable'		=> array('BOOL', 1),
-						'article_views'			=> array('UINT', 0),
-						'article_time'			=> array('TIMESTAMP', 0),
-					),
+		return [
+			'add_tables' => [
+				$this->table_prefix . 'portal_categories' => [
+					'COLUMNS' => [
+						'cat_id'		=> ['UINT', null, 'auto_increment'],
+						'parent_id'		=> ['UINT', 0],
+						'left_id'		=> ['UINT', 0],
+						'right_id'		=> ['UINT', 0],
+						'cat_name'		=> ['VCHAR_UNI', ''],
+						'cat_name_vi'	=> ['VCHAR_UNI', ''],
+						'cat_varname'	=> ['VCHAR', ''],
+						'cat_icon'		=> ['VCHAR', '']
+					],
+					'PRIMARY_KEY' => 'cat_id'
+				],
+				$this->table_prefix . 'portal_articles' => [
+					'COLUMNS' => [
+						'article_id'			=> ['UINT', null, 'auto_increment'],
+						'cat_id'				=> ['UINT', 0],
+						'article_name'			=> ['VCHAR_UNI', ''],
+						'article_name_seo'		=> ['VCHAR', ''],
+						'article_lang'			=> ['VCHAR:30', ''],
+						'article_img'			=> ['VCHAR', ''],
+						'article_desc'			=> ['TEXT_UNI', ''],
+						'article_text'			=> ['MTEXT_UNI', ''],
+						'article_text_uid'		=> ['VCHAR:8', ''],
+						'article_text_bitfield'	=> ['VCHAR:255', ''],
+						'article_text_options'	=> ['UINT:11', 0],
+						'article_enable'		=> ['BOOL', 1],
+						'article_views'			=> ['UINT', 0],
+						'article_time'			=> ['TIMESTAMP', 0]
+					],
 					'PRIMARY_KEY' => 'article_id',
-					'KEYS' => array(
-						'c_id'	=> array('INDEX', 'cat_id'),
-					),
-				),
-				$this->table_prefix . 'portal_comments' => array(
-					'COLUMNS' => array(
-						'comment_id'			=> array('UINT', null, 'auto_increment'),
-						'user_id'				=> array('ULINT', 0),
-						'article_id'			=> array('UINT', 0),
-						'comment_rate'			=> array('TINT:1', 0),
-						'comment_text'			=> array('TEXT_UNI', ''),
-						'comment_text_uid'		=> array('VCHAR:8', ''),
-						'comment_text_bitfield'	=> array('VCHAR:255', ''),
-						'comment_text_options'	=> array('UINT:11', 0),
-						'comment_mode'			=> array('TINT:1', 0),
-						'comment_time'			=> array('TIMESTAMP', 0),
-					),
+					'KEYS' => [
+						'c_id'	=> ['INDEX', 'cat_id']
+					],
+				],
+				$this->table_prefix . 'portal_comments' => [
+					'COLUMNS' => [
+						'comment_id'			=> ['UINT', null, 'auto_increment'],
+						'user_id'				=> ['ULINT', 0],
+						'article_id'			=> ['UINT', 0],
+						'comment_rate'			=> ['TINT:1', 0],
+						'comment_text'			=> ['TEXT_UNI', ''],
+						'comment_text_uid'		=> ['VCHAR:8', ''],
+						'comment_text_bitfield'	=> ['VCHAR:255', ''],
+						'comment_text_options'	=> ['UINT:11', 0],
+						'comment_mode'			=> ['TINT:1', 0],
+						'comment_time'			=> ['TIMESTAMP', 0]
+					],
 					'PRIMARY_KEY' => 'comment_id',
-					'KEYS' => array(
-						'u_id'	=> array('INDEX', 'user_id'),
-						'a_id'	=> array('INDEX', 'article_id'),
-					),
-				),
-			),
-		);
+					'KEYS' => [
+						'u_id'	=> ['INDEX', 'user_id'],
+						'a_id'	=> ['INDEX', 'article_id']
+					]
+				]
+			]
+		];
 	}
 
+	/**
+	* Revert schema
+	*
+	* @return array
+	*/
 	public function revert_schema()
 	{
-		return array(
-			'drop_tables' => array(
+		return [
+			'drop_tables' => [
 				$this->table_prefix . 'portal_categories',
 				$this->table_prefix . 'portal_articles',
-				$this->table_prefix . 'portal_comments',
-			),
-		);
+				$this->table_prefix . 'portal_comments'
+			]
+		];
 	}
 }
