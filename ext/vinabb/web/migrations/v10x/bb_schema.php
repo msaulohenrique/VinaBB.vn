da5a86a90678018a89f2fb696e330618e0450ccf
Khoa Nguyen <nedkavn@gmail.com>
2016-11-07 19:56:18 +0700

    phpBB Resource: Update schema for nested-categories

--- ext/vinabb/web/migrations/v10x/bb_schema.php
+++ ext/vinabb/web/migrations/v10x/bb_schema.php
@@ -10,108 +10,123 @@ namespace vinabb\web\migrations\v10x;
 
 use phpbb\db\migration\migration;
 
+/**
+* Database schema for phpBB Resource
+*/
 class bb_schema extends migration
 {
+	/**
+	* Update schema
+	*
+	* @return array
+	*/
 	public function update_schema()
 	{
-		return array(
-			'add_tables' => array(
-				$this->table_prefix . 'bb_categories' => array(
-					'COLUMNS' => array(
-						'cat_id'		=> array('UINT', null, 'auto_increment'),
-						'bb_type'		=> array('TINT:1', 0),
-						'cat_name'		=> array('VCHAR_UNI', ''),
-						'cat_name_vi'	=> array('VCHAR_UNI', ''),
-						'cat_varname'	=> array('VCHAR', ''),
-						'cat_desc'		=> array('TEXT_UNI', ''),
-						'cat_desc_vi'	=> array('TEXT_UNI', ''),
-						'cat_icon'		=> array('VCHAR', ''),
-						'cat_order'		=> array('USINT', 0),
-					),
+		return [
+			'add_tables' => [
+				$this->table_prefix . 'bb_categories' => [
+					'COLUMNS' => [
+						'cat_id'		=> ['UINT', null, 'auto_increment'],
+						'parent_id'		=> ['UINT', 0],
+						'left_id'		=> ['UINT', 0],
+						'right_id'		=> ['UINT', 0],
+						'bb_type'		=> ['TINT:1', 0],
+						'cat_name'		=> ['VCHAR_UNI', ''],
+						'cat_name_vi'	=> ['VCHAR_UNI', ''],
+						'cat_varname'	=> ['VCHAR', ''],
+						'cat_desc'		=> ['TEXT_UNI', ''],
+						'cat_desc_vi'	=> ['TEXT_UNI', ''],
+						'cat_icon'		=> ['VCHAR', '']
+					],
 					'PRIMARY_KEY' => 'cat_id',
-					'KEYS' => array(
-						'bb'	=> array('INDEX', 'bb_type'),
-					),
-				),
-				$this->table_prefix . 'bb_items' => array(
-					'COLUMNS' => array(
-						'item_id'					=> array('UINT', null, 'auto_increment'),
-						'bb_type'					=> array('TINT:1', 0),
-						'cat_id'					=> array('UINT', 0),
-						'author_id'					=> array('UINT', 0),
-						'item_name'					=> array('VCHAR_UNI', '', 'true_sort'),
-						'item_varname'				=> array('VCHAR', ''),
-						'item_version'				=> array('VCHAR', ''),
-						'item_phpbb_version'		=> array('VCHAR', ''),
-						'item_desc'					=> array('TEXT_UNI', ''),
-						'item_desc_uid'				=> array('VCHAR:8', ''),
-						'item_desc_bitfield'		=> array('VCHAR:255', ''),
-						'item_desc_options'			=> array('UINT:11', 0),
-						'item_desc_vi'				=> array('TEXT_UNI', ''),
-						'item_desc_vi_uid'			=> array('VCHAR:8', ''),
-						'item_desc_vi_bitfield'		=> array('VCHAR:255', ''),
-						'item_desc_vi_options'		=> array('UINT:11', 0),
-						'item_ext_style'			=> array('BOOL', 0),
-						'item_ext_acp_style'		=> array('BOOL', 0),
-						'item_ext_lang'				=> array('BOOL', 0),
-						'item_ext_db_schema'		=> array('BOOL', 0),
-						'item_ext_db_data'			=> array('BOOL', 0),
-						'item_style_presets'		=> array('UINT', 0),
-						'item_style_presets_aio'	=> array('BOOL', 0),
-						'item_style_source'			=> array('BOOL', 0),
-						'item_style_responsive'		=> array('BOOL', 0),
-						'item_style_bootstrap'		=> array('BOOL', 0),
-						'item_lang_iso'				=> array('VCHAR:30', ''),
-						'item_tool_os'				=> array('TINT:2', 0),
-						'item_price'				=> array('BINT', 0),
-						'item_url'					=> array('VCHAR', ''),
-						'item_github'				=> array('VCHAR', ''),
-						'item_added'				=> array('TIMESTAMP', 0),
-						'item_updated'				=> array('TIMESTAMP', 0),
-					),
+					'KEYS' => [
+						'bb'	=> ['INDEX', 'bb_type']
+					]
+				],
+				$this->table_prefix . 'bb_items' => [
+					'COLUMNS' => [
+						'item_id'					=> ['UINT', null, 'auto_increment'],
+						'bb_type'					=> ['TINT:1', 0],
+						'cat_id'					=> ['UINT', 0],
+						'author_id'					=> ['UINT', 0],
+						'item_name'					=> ['VCHAR_UNI', '', 'true_sort'],
+						'item_varname'				=> ['VCHAR', ''],
+						'item_version'				=> ['VCHAR', ''],
+						'item_phpbb_version'		=> ['VCHAR', ''],
+						'item_desc'					=> ['TEXT_UNI', ''],
+						'item_desc_uid'				=> ['VCHAR:8', ''],
+						'item_desc_bitfield'		=> ['VCHAR:255', ''],
+						'item_desc_options'			=> ['UINT:11', 0],
+						'item_desc_vi'				=> ['TEXT_UNI', ''],
+						'item_desc_vi_uid'			=> ['VCHAR:8', ''],
+						'item_desc_vi_bitfield'		=> ['VCHAR:255', ''],
+						'item_desc_vi_options'		=> ['UINT:11', 0],
+						'item_ext_style'			=> ['BOOL', 0],
+						'item_ext_acp_style'		=> ['BOOL', 0],
+						'item_ext_lang'				=> ['BOOL', 0],
+						'item_ext_db_schema'		=> ['BOOL', 0],
+						'item_ext_db_data'			=> ['BOOL', 0],
+						'item_style_presets'		=> ['UINT', 0],
+						'item_style_presets_aio'	=> ['BOOL', 0],
+						'item_style_source'			=> ['BOOL', 0],
+						'item_style_responsive'		=> ['BOOL', 0],
+						'item_style_bootstrap'		=> ['BOOL', 0],
+						'item_lang_iso'				=> ['VCHAR:30', ''],
+						'item_tool_os'				=> ['TINT:2', 0],
+						'item_price'				=> ['BINT', 0],
+						'item_url'					=> ['VCHAR', ''],
+						'item_github'				=> ['VCHAR', ''],
+						'item_added'				=> ['TIMESTAMP', 0],
+						'item_updated'				=> ['TIMESTAMP', 0]
+					],
 					'PRIMARY_KEY' => 'item_id',
-					'KEYS' => array(
-						'bb'	=> array('INDEX', 'bb_type'),
-						'c_id'	=> array('INDEX', 'cat_id'),
-						'a_id'	=> array('INDEX', 'author_id'),
-					),
-				),
-				$this->table_prefix . 'bb_authors' => array(
-					'COLUMNS' => array(
-						'author_id'				=> array('UINT', null, 'auto_increment'),
-						'user_id'				=> array('ULINT', 0),
-						'author_name'			=> array('VCHAR_UNI', ''),
-						'author_firstname'		=> array('VCHAR_UNI', ''),
-						'author_lastname'		=> array('VCHAR_UNI', ''),
-						'author_is_group'		=> array('BOOL', 0),
-						'author_group'			=> array('UINT', 0),
-						'author_www'			=> array('VCHAR_UNI', 0),
-						'author_email'			=> array('VCHAR_UNI', 0),
-						'author_phpbb'			=> array('ULINT', 0),
-						'author_github'			=> array('VCHAR', ''),
-						'author_facebook'		=> array('VCHAR', ''),
-						'author_twitter'		=> array('VCHAR', ''),
-						'author_google'			=> array('VCHAR', ''),
-						'author_google_plus'	=> array('VCHAR', ''),
-						'author_skype'			=> array('VCHAR', ''),
-					),
+					'KEYS' => [
+						'bb'	=> ['INDEX', 'bb_type'],
+						'c_id'	=> ['INDEX', 'cat_id'],
+						'a_id'	=> ['INDEX', 'author_id']
+					]
+				],
+				$this->table_prefix . 'bb_authors' => [
+					'COLUMNS' => [
+						'author_id'				=> ['UINT', null, 'auto_increment'],
+						'user_id'				=> ['ULINT', 0],
+						'author_name'			=> ['VCHAR_UNI', ''],
+						'author_firstname'		=> ['VCHAR_UNI', ''],
+						'author_lastname'		=> ['VCHAR_UNI', ''],
+						'author_is_group'		=> ['BOOL', 0],
+						'author_group'			=> ['UINT', 0],
+						'author_www'			=> ['VCHAR_UNI', 0],
+						'author_email'			=> ['VCHAR_UNI', 0],
+						'author_phpbb'			=> ['ULINT', 0],
+						'author_github'			=> ['VCHAR', ''],
+						'author_facebook'		=> ['VCHAR', ''],
+						'author_twitter'		=> ['VCHAR', ''],
+						'author_google'			=> ['VCHAR', ''],
+						'author_google_plus'	=> ['VCHAR', ''],
+						'author_skype'			=> ['VCHAR', '']
+					],
 					'PRIMARY_KEY' => 'author_id',
-					'KEYS' => array(
-						'u_id'	=> array('INDEX', 'user_id'),
-					),
-				),
-			),
-		);
+					'KEYS' => [
+						'u_id'	=> ['INDEX', 'user_id']
+					]
+				]
+			]
+		];
 	}
 
+	/**
+	* Revert schema
+	*
+	* @return array
+	*/
 	public function revert_schema()
 	{
-		return array(
-			'drop_tables' => array(
+		return [
+			'drop_tables' => [
 				$this->table_prefix . 'bb_categories',
 				$this->table_prefix . 'bb_items',
-				$this->table_prefix . 'bb_authors',
-			),
-		);
+				$this->table_prefix . 'bb_authors'
+			]
+		];
 	}
 }
