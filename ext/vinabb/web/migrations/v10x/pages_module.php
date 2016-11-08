2f9cd71c3327af97a52134e7286e980d958fa390
Khoa Nguyen <nedkavn@gmail.com>
2016-11-09 02:40:27 +0700

    Migration: Add new ACP modules

---
+++ ext/vinabb/web/migrations/v10x/pages_module.php
@@ -0,0 +1,49 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\web\migrations\v10x;
+
+use phpbb\db\migration\migration;
+
+/**
+* Add ACP module for menu manager
+*/
+class pages_module extends migration
+{
+	/**
+	* List of required migrations
+	*
+	* @return array
+	*/
+	static public function depends_on()
+	{
+		return ['\vinabb\web\migrations\v10x\portal_categories'];
+	}
+
+	/**
+	* Update data
+	*
+	* @return array
+	*/
+	public function update_data()
+	{
+		return [
+			[
+				'module.add',
+				[
+					'acp',
+					'ACP_CAT_PORTAL',
+					[
+						'module_basename'	=> '\vinabb\web\acp\pages_module',
+						'modes'				=> ['main']
+					]
+				]
+			]
+		];
+	}
+}
