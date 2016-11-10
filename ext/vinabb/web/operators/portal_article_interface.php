ec175fed2abe1e34559b2d5eb0e87c75f32ff778
Khoa Nguyen <nedkavn@gmail.com>
2016-11-11 03:30:48 +0700

    Portal: Add interface for the article operators class

---
+++ ext/vinabb/web/operators/portal_article_interface.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\web\operators;
+
+/**
+* Interface for a set of articles
+*/
+interface portal_article_interface
+{
+	/**
+	* Get all articles
+	*
+	* @return array
+	*/
+	public function get_articles();
+
+	/**
+	* Add an article
+	*
+	* @return \vinabb\web\entities\portal_article_interface
+	*/
+	public function add_article();
+
+	/**
+	* Delete an article
+	*
+	* @param int $id Article ID
+	* @return bool True if row was deleted, false otherwise
+	* @throws \vinabb\web\exceptions\out_of_bounds
+	*/
+	public function delete_article($id);
+}
