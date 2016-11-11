66a985036d70898f54dd2807ef0ac43a9fc3e981
Khoa Nguyen <nedkavn@gmail.com>
2016-11-11 10:21:31 +0700

    phpBB Resource: Add interface for the author operators class

---
+++ ext/vinabb/web/operators/bb_author_interface.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\web\operators;
+
+/**
+* Interface for a set of phpBB resource authors
+*/
+interface bb_author_interface
+{
+	/**
+	* Get all authors
+	*
+	* @return array
+	*/
+	public function get_authors();
+
+	/**
+	* Add an author
+	*
+	* @return \vinabb\web\entities\bb_author_interface
+	*/
+	public function add_author();
+
+	/**
+	* Delete an author
+	*
+	* @param int $id Author ID
+	* @return bool True if row was deleted, false otherwise
+	* @throws \vinabb\web\exceptions\out_of_bounds
+	*/
+	public function delete_author($id);
+}
