109b85f8ffcc44caca125d3da85ff2d1260b5edb
Khoa Nguyen <nedkavn@gmail.com>
2016-11-13 15:58:29 +0700

    Headlines: Add ACP module files

---
+++ ext/vinabb/web/acp/headlines_info.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\web\acp;
+
+/**
+* ACP module info: acp_headlines
+*/
+class headlines_info
+{
+	public function module()
+	{
+		return [
+			'filename'	=> '\vinabb\web\acp\headlines_module',
+			'title'		=> 'ACP_HEADLINES',
+			'version'	=> '1.0.0',
+			'modes'		=> [
+				'main'	=> [
+					'title'	=> 'ACP_HEADLINES',
+					'auth'	=> 'ext_vinabb/web && acl_a_board',
+					'cat'	=> ['ACP_CAT_PORTAL']
+				]
+			]
+		];
+	}
+}
---
+++ ext/vinabb/web/acp/headlines_module.php
@@ -0,0 +1,106 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\web\acp;
+
+/**
+* ACP module: acp_headlines
+*/
+class headlines_module
+{
+	/** @var \vinabb\web\controllers\acp\headlines_interface */
+	protected $controller;
+
+	/** @var \phpbb\language\language */
+	protected $language;
+
+	/** @var \phpbb\request\request */
+	protected $request;
+
+	/** @var string */
+	public $tpl_name;
+
+	/** @var string */
+	public $page_title;
+
+	/** @var string */
+	public $u_action;
+
+	/**
+	* Main method of the module
+	*
+	* @param string	$id		Module basename
+	* @param string	$mode	Module mode
+	*/
+	public function main($id, $mode)
+	{
+		global $phpbb_container;
+
+		$this->controller = $phpbb_container->get('vinabb.web.acp.headlines');
+		$this->language = $phpbb_container->get('language');
+		$this->request = $phpbb_container->get('request');
+
+		// ACP template file
+		$this->tpl_name = 'acp_headlines';
+		$this->page_title = $this->language->lang('ACP_HEADLINES');
+
+		// Language
+		$this->language->add_lang('acp_menus', 'vinabb/web');
+
+		// Requests
+		$action = $this->request->variable('action', '');
+		$headline_id = $this->request->variable('id', 0);
+
+		$this->controller->set_form_action($this->u_action);
+
+		// Do actions via the controller
+		switch ($action)
+		{
+			case 'add':
+				$this->page_title = $this->language->lang('ADD_HEADLINE');
+				$this->controller->add_headline();
+
+				// Return to stop execution of this script
+				return;
+
+			case 'edit':
+				$this->page_title = $this->language->lang('EDIT_HEADLINE');
+				$this->controller->edit_headline($headline_id);
+
+				// Return to stop execution of this script
+				return;
+
+			case 'move_down':
+				$this->controller->move_headline($headline_id, 'down');
+			break;
+
+			case 'move_up':
+				$this->controller->move_headline($headline_id, 'up');
+			break;
+
+			case 'delete':
+				if (confirm_box(true))
+				{
+					$this->controller->delete_headline($headline_id);
+				}
+				else
+				{
+					confirm_box(false, $this->language->lang('CONFIRM_DELETE_HEADLINE'), build_hidden_fields([
+						'i'			=> $id,
+						'mode'		=> $mode,
+						'action'	=> $action,
+						'id'		=> $headline_id
+					]));
+				}
+			break;
+		}
+
+		// Manage menus
+		$this->controller->display_headlines();
+	}
+}
