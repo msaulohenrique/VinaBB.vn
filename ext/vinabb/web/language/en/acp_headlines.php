e8f7b7cb6efafb73dadbb7fba6e4814b021e34aa
Khoa Nguyen <nedkavn@gmail.com>
2016-11-15 17:25:31 +0700

    Headline: Add language files

---
+++ ext/vinabb/web/language/en/acp_headlines.php
@@ -0,0 +1,45 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+/**
+* All language files should use UTF-8 as their encoding
+* and the files must not contain a BOM.
+*/
+
+$lang = array_merge($lang, [
+	'ADD_HEADLINE'	=> 'Create new headline',
+
+	'CONFIRM_DELETE_HEADLINE'	=> 'Are you sure you want to delete this headline?',
+
+	'EDIT_HEADLINE'						=> 'Edit headline',
+	'ERROR_HEADLINE_DESC_EMPTY'			=> 'You must enter a description.',
+	'ERROR_HEADLINE_DESC_TOO_LONG'		=> 'The description is too long.',
+	'ERROR_HEADLINE_IMG_EMPTY'			=> 'You must select a headline image.',
+	'ERROR_HEADLINE_LANG_EMPTY'			=> 'You must select a language.',
+	'ERROR_HEADLINE_LANG_NOT_EXISTS'	=> 'The language does not exist.',
+	'ERROR_HEADLINE_NAME_EMPTY'			=> 'You must enter a title.',
+	'ERROR_HEADLINE_NAME_TOO_LONG'		=> 'The title is too long.',
+	'ERROR_HEADLINE_URL_EMPTY'			=> 'You must enter an URL.',
+
+	'HEADLINE_DESC'	=> 'Description',
+	'HEADLINE_IMG'	=> 'Headline image',
+	'HEADLINE_NAME'	=> 'Title',
+	'HEADLINE_URL'	=> 'Headline URL',
+
+	'MESSAGE_HEADLINE_ADD'		=> 'The headline has been created.',
+	'MESSAGE_HEADLINE_DELETE'	=> 'The headline has been deleted.',
+	'MESSAGE_HEADLINE_EDIT'		=> 'The headline has been edited.'
+]);
---
+++ ext/vinabb/web/language/vi/acp_headlines.php
@@ -0,0 +1,45 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+/**
+* All language files should use UTF-8 as their encoding
+* and the files must not contain a BOM.
+*/
+
+$lang = array_merge($lang, [
+	'ADD_HEADLINE'	=> 'Tạo quảng cáo mới',
+
+	'CONFIRM_DELETE_HEADLINE'	=> 'Are you sure you want to delete this headline?',
+
+	'EDIT_HEADLINE'						=> 'Sửa quảng cáo',
+	'ERROR_HEADLINE_DESC_EMPTY'			=> 'Chưa nhập giới thiệu.',
+	'ERROR_HEADLINE_DESC_TOO_LONG'		=> 'Giới thiệu quá dài.',
+	'ERROR_HEADLINE_IMG_EMPTY'			=> 'Chưa chọn hình quảng cáo.',
+	'ERROR_HEADLINE_LANG_EMPTY'			=> 'Chưa chọn ngôn ngữ.',
+	'ERROR_HEADLINE_LANG_NOT_EXISTS'	=> 'Ngôn ngữ không tồn tại.',
+	'ERROR_HEADLINE_NAME_EMPTY'			=> 'Chưa nhập tiêu đề.',
+	'ERROR_HEADLINE_NAME_TOO_LONG'		=> 'Tiêu đề quá dài.',
+	'ERROR_HEADLINE_URL_EMPTY'			=> 'Chưa nhập liên kết.',
+
+	'HEADLINE_DESC'	=> 'Giới thiệu',
+	'HEADLINE_IMG'	=> 'Hình quảng cáo',
+	'HEADLINE_NAME'	=> 'Tiêu đề',
+	'HEADLINE_URL'	=> 'Liên kết',
+
+	'MESSAGE_HEADLINE_ADD'		=> 'Đã tạo quảng cáo.',
+	'MESSAGE_HEADLINE_DELETE'	=> 'Đã xóa quảng cáo.',
+	'MESSAGE_HEADLINE_EDIT'		=> 'Đã sửa quảng cáo.'
+]);
