8ae597d703f4dda67d16e82c009246bd1f46e57b
Khoa Nguyen <nedkavn@gmail.com>
2016-11-15 17:25:05 +0700

    Pages: Update language files

--- ext/vinabb/web/language/en/acp_pages.php
+++ ext/vinabb/web/language/en/acp_pages.php
@@ -20,22 +20,22 @@ if (empty($lang) || !is_array($lang))
 */
 
 $lang = array_merge($lang, [
-	'ADD_PAGE'			=> 'Create new page',
+	'ADD_PAGE'	=> 'Create new page',
 
 	'CONFIRM_DELETE_PAGE'	=> 'Are you sure you want to delete this page?',
 
 	'EDIT_PAGE'						=> 'Edit page',
 	'ERROR_PAGE_NAME_EMPTY'			=> 'You must enter a page title.',
+	'ERROR_PAGE_NAME_TOO_LONG'		=> 'The page title is too long.',
+	'ERROR_PAGE_NAME_VI_TOO_LONG'	=> 'The Vietnamese page title is too long.',
 	'ERROR_PAGE_VARNAME_DUPLICATE'	=> 'The page varname “%s” already exists.',
 	'ERROR_PAGE_VARNAME_EMPTY'		=> 'You must enter a page varname.',
 	'ERROR_PAGE_VARNAME_INVALID'	=> 'The page varname is invalid.',
+	'ERROR_PAGE_VARNAME_TOO_LONG'	=> 'The page varname is too long.',
 
 	'MESSAGE_PAGE_ADD'		=> 'The page has been created.',
 	'MESSAGE_PAGE_DELETE'	=> 'The page has been deleted.',
 	'MESSAGE_PAGE_EDIT'		=> 'The page has been edited.',
-
-	'NO_PAGE'		=> 'The page does not exist.',
-	'NO_PAGE_ID'	=> 'No pages specified.',
 
 	'PAGE_DESC'		=> 'Description',
 	'PAGE_NAME'		=> 'Page title',
--- ext/vinabb/web/language/vi/acp_pages.php
+++ ext/vinabb/web/language/vi/acp_pages.php
@@ -20,22 +20,22 @@ if (empty($lang) || !is_array($lang))
 */
 
 $lang = array_merge($lang, [
-	'ADD_PAGE'			=> 'Tạo trang mới',
+	'ADD_PAGE'	=> 'Tạo trang mới',
 
 	'CONFIRM_DELETE_PAGE'	=> 'Bạn chắc chắn muốn xóa trang này?',
 
 	'EDIT_PAGE'						=> 'Sửa trang',
 	'ERROR_PAGE_NAME_EMPTY'			=> 'Chưa nhập tiêu đề trang.',
+	'ERROR_PAGE_NAME_TOO_LONG'		=> 'Tiêu đề trang quá dài.',
+	'ERROR_PAGE_NAME_VI_TOO_LONG'	=> 'Tiêu đề trang tiếng Việt quá dài.',
 	'ERROR_PAGE_VARNAME_DUPLICATE'	=> 'Tên biến trang “%s” bị trùng.',
 	'ERROR_PAGE_VARNAME_EMPTY'		=> 'Chưa nhập tên biến trang.',
 	'ERROR_PAGE_VARNAME_INVALID'	=> 'Tên biến trang không hợp lệ.',
+	'ERROR_PAGE_VARNAME_TOO_LONG'	=> 'Tên biến trang quá dài.',
 
 	'MESSAGE_PAGE_ADD'		=> 'Đã tạo trang.',
 	'MESSAGE_PAGE_DELETE'	=> 'Đã xóa trang.',
 	'MESSAGE_PAGE_EDIT'		=> 'Đã sửa trang.',
-
-	'NO_PAGE'		=> 'Trang không tồn tại.',
-	'NO_PAGE_ID'	=> 'Trang không xác định.',
 
 	'PAGE_DESC'		=> 'Giới thiệu',
 	'PAGE_NAME'		=> 'Tiêu đề trang',
