722e702403d095632ff51364623429664b8f2427
Khoa Nguyen <nedkavn@gmail.com>
2016-11-15 17:25:42 +0700

    Menu Manager: Add language files

---
+++ ext/vinabb/web/language/en/acp_menus.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+/**
+* All language files should use UTF-8 as their encoding
+* and the files must not contain a BOM.
+*/
+
+$lang = array_merge($lang, [
+	'ADD_MENU'	=> 'Create new menu',
+
+	'CONFIRM_DELETE_MENU'	=> 'Are you sure you want to delete this menu?',
+
+	'EDIT_MENU'						=> 'Edit menu',
+	'ERROR_MENU_NAME_EMPTY'			=> 'You must enter a menu title.',
+	'ERROR_MENU_NAME_TOO_LONG'		=> 'The menu title is too long.',
+	'ERROR_MENU_NAME_VI_TOO_LONG'	=> 'The Vietnamese menu title is too long.',
+
+	'MENU_LINK'				=> 'Menu link',
+	'MENU_NAME'				=> 'Menu title',
+	'MENU_TARGET'			=> 'Open in new tab',
+	'MENU_TYPE'				=> 'Menu type',
+	'MESSAGE_MENU_ADD'		=> 'The menu has been created.',
+	'MESSAGE_MENU_DELETE'	=> 'The menu has been deleted.',
+	'MESSAGE_MENU_EDIT'		=> 'The menu has been edited.'
+]);
---
+++ ext/vinabb/web/language/vi/acp_menus.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+/**
+* All language files should use UTF-8 as their encoding
+* and the files must not contain a BOM.
+*/
+
+$lang = array_merge($lang, [
+	'ADD_MENU'	=> 'Tạo trình đơn mới',
+
+	'CONFIRM_DELETE_MENU'	=> 'Bạn chắc chắn muốn xóa trình đơn này?',
+
+	'EDIT_MENU'						=> 'Sửa trình đơn',
+	'ERROR_MENU_NAME_EMPTY'			=> 'Chưa nhập tên trình đơn.',
+	'ERROR_MENU_NAME_TOO_LONG'		=> 'Tên trình đơn quá dài.',
+	'ERROR_MENU_NAME_VI_TOO_LONG'	=> 'Tên trình đơn tiếng Việt quá dài.',
+
+	'MENU_LINK'				=> 'Liên kết',
+	'MENU_NAME'				=> 'Tên trình đơn',
+	'MENU_TARGET'			=> 'Mở trang mới',
+	'MENU_TYPE'				=> 'Loại trình đơn',
+	'MESSAGE_MENU_ADD'		=> 'Đã tạo trình đơn.',
+	'MESSAGE_MENU_DELETE'	=> 'Đã xóa trình đơn.',
+	'MESSAGE_MENU_EDIT'		=> 'Đã sửa trình đơn.'
+]);
