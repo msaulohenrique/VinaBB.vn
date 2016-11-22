bc61e9b19fa8fc54ea5e8da630e7a23b84e656cb
Khoa Nguyen <nedkavn@gmail.com>
2016-11-22 08:18:55 +0700

    BB Category Entity: Simplify the method import()

--- ext/vinabb/web/entities/bb_category.php
+++ ext/vinabb/web/entities/bb_category.php
@@ -15,22 +15,6 @@ use vinabb\web\includes\constants;
 */
 class bb_category implements bb_category_interface
 {
-	/**
-	* Data for this entity
-	*
-	* @var array
-	*	cat_id
-	*	bb_type
-	*	cat_name
-	*	cat_name_vi
-	*	cat_varname
-	*	cat_desc
-	*	cat_desc_vi
-	*	cat_icon
-	*	cat_order
-	*/
-	protected $data;
-
 	/** @var \phpbb\db\driver\driver_interface */
 	protected $db;
 
@@ -39,6 +23,9 @@ class bb_category implements bb_category_interface
 
 	/** @var string */
 	protected $table_name;
+
+	/** @var array */
+	protected $data;
 
 	/**
 	* Constructor
@@ -52,6 +39,26 @@ class bb_category implements bb_category_interface
 		$this->db = $db;
 		$this->entity_helper = $entity_helper;
 		$this->table_name = $table_name;
+	}
+
+	/**
+	* Data for this entity
+	*
+	* @return array
+	*/
+	protected function prepare_data()
+	{
+		return [
+			'cat_id'		=> 'integer',
+			'bb_type'		=> 'integer',
+			'cat_name'		=> 'string',
+			'cat_name_vi'	=> 'string',
+			'cat_varname'	=> 'string',
+			'cat_desc'		=> 'string',
+			'cat_desc_vi'	=> 'string',
+			'cat_icon'		=> 'string',
+			'cat_order'		=> 'integer'
+		];
 	}
 
 	/**
@@ -95,55 +102,27 @@ class bb_category implements bb_category_interface
 		// Clear out any saved data
 		$this->data = [];
 
-		// All of our fields
-		$fields = [
-			'cat_id'		=> 'integer',
-			'bb_type'		=> 'integer',
-			'cat_name'		=> 'set_name',
-			'cat_name_vi'	=> 'set_name_vi',
-			'cat_varname'	=> 'set_varname',
-			'cat_desc'		=> 'set_desc',
-			'cat_desc_vi'	=> 'set_desc_vi',
-			'cat_icon'		=> 'set_icon',
-			'cat_order'		=> 'integer'
-		];
-
 		// Go through the basic fields and set them to our data array
-		foreach ($fields as $field => $type)
+		foreach ($this->prepare_data() as $field => $type)
 		{
 			// The data wasn't sent to us
 			if (!isset($data[$field]))
 			{
 				throw new \vinabb\web\exceptions\invalid_argument([$field, 'EMPTY']);
 			}
-
-			// If the type is a method on this class, call it
-			if (method_exists($this, $type))
-			{
-				$this->$type($data[$field]);
-			}
-			else
-			{
-				// settype() passes values by reference
-				$value = $data[$field];
-
-				// We're using settype() to enforce data types
-				settype($value, $type);
-
-				$this->data[$field] = $value;
-			}
-		}
-
-		// Some fields must be >= 0
-		$validate_unsigned = ['cat_id', 'bb_type', 'cat_order'];
-
-		foreach ($validate_unsigned as $field)
-		{
-			// If the data is less than 0, it's not unsigned and we'll throw an exception
-			if ($this->data[$field] < 0)
+			// We love unsigned numbers
+			else if ($type != 'string' && $this->data[$field] < 0)
 			{
 				throw new \vinabb\web\exceptions\out_of_bounds($field);
 			}
+
+			// settype() passes values by reference
+			$value = $data[$field];
+
+			// We're using settype() to enforce data types
+			settype($value, $type);
+
+			$this->data[$field] = $value;
 		}
 
 		return $this;
