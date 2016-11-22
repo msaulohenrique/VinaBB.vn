2abd094861e8e8ecf99fab43f490ec0fb068080d
Khoa Nguyen <nedkavn@gmail.com>
2016-11-22 20:35:55 +0700

    Add interface for the topic operators class

---
+++ ext/vinabb/web/operators/topic_interface.php
@@ -0,0 +1,67 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\web\operators;
+
+/**
+* Interface for a set of topics
+*/
+interface topic_interface
+{
+	/**
+	* Get number of topics
+	*
+	* @param int $forum_id Forum ID
+	* @return int
+	*/
+	public function count_topics($forum_id = 0);
+
+	/**
+	* Get all topics
+	*
+	* @return array
+	*/
+	public function get_topics();
+
+	/**
+	* Get topics in range for pagination
+	*
+	* @param int	$forum_id		Forum ID
+	* @param string	$order_field	Sort by this field
+	* @param int	$limit			Number of items
+	* @param int	$offset			Position of the start
+	* @return array
+	*/
+	public function list_topics($forum_id = 0, $order_field = 'topic_time DESC', $limit = 0, $offset = 0);
+
+	/**
+	* Get latest topics
+	*
+	* @param int	$forum_id	Forum ID
+	* @param int	$limit		Number of items
+	* @return array
+	*/
+	public function get_latest_topics($forum_id = 0, $limit = 10);
+
+	/**
+	* Add a topic
+	*
+	* @param \vinabb\web\entities\topic_interface $entity Topic entity
+	* @return \vinabb\web\entities\topic_interface
+	*/
+	public function add_topic(\vinabb\web\entities\topic_interface $entity);
+
+	/**
+	* Delete a topic
+	*
+	* @param int $id Topic ID
+	* @return bool True if row was deleted, false otherwise
+	* @throws \vinabb\web\exceptions\out_of_bounds
+	*/
+	public function delete_topic($id);
+}
