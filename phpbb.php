38791e5e447663bf45ab30b5d6b63170131e3055
Khoa Nguyen <nedkavn@gmail.com>
2016-11-22 09:13:48 +0700

    Front files: Add the header file

--- phpbb.php
+++ phpbb.php
@@ -1,3 +1,9 @@
 <?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
 
 define('IN_PHPBB', true);
