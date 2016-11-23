c66383457ad88642e3fe1b4462aa542bf070b8fa
Khoa Nguyen <nedkavn@gmail.com>
2016-11-23 16:42:32 +0700

    Setting ACP Controller: Move tasks to the new helper

--- ext/vinabb/web/controllers/acp/settings.php
+++ ext/vinabb/web/controllers/acp/settings.php
@@ -13,7 +13,7 @@ use vinabb\web\includes\constants;
 /**
 * Controller for the settings_module
 */
-class settings
+class settings implements settings_interface
 {
 	/** @var \phpbb\auth\auth */
 	protected $auth;
@@ -26,9 +26,6 @@ class settings
 
 	/** @var \phpbb\config\db_text */
 	protected $config_text;
-
-	/** @var \phpbb\db\driver\driver_interface */
-	protected $db;
 
 	/** @var \phpbb\language\language */
 	protected $language;
@@ -44,6 +41,9 @@ class settings
 
 	/** @var \phpbb\user */
 	protected $user;
+
+	/** @var \vinabb\web\controllers\acp\helper\setting_tasks_interface */
+	protected $task_helper;
 
 	/** @var string */
 	protected $u_action;
@@ -66,40 +66,40 @@ class settings
 	/**
 	* Constructor
 	*
-	* @param \phpbb\auth\auth									$auth			Authentication object
-	* @param \vinabb\web\controllers\cache\service_interface	$cache			Cache service
-	* @param \phpbb\config\config								$config			Config object
-	* @param \phpbb\config\db_text								$config_text	Config text object
-	* @param \phpbb\db\driver\driver_interface					$db				Database object
-	* @param \phpbb\language\language							$language		Language object
-	* @param \phpbb\log\log										$log			Log object
-	* @param \phpbb\request\request								$request		Request object
-	* @param \phpbb\template\template							$template		Template object
-	* @param \phpbb\user										$user			User object
+	* @param \phpbb\auth\auth												$auth			Authentication object
+	* @param \vinabb\web\controllers\cache\service_interface				$cache			Cache service
+	* @param \phpbb\config\config											$config			Config object
+	* @param \phpbb\config\db_text											$config_text	Config text object
+	* @param \phpbb\language\language										$language		Language object
+	* @param \phpbb\log\log													$log			Log object
+	* @param \phpbb\request\request											$request		Request object
+	* @param \phpbb\template\template										$template		Template object
+	* @param \phpbb\user													$user			User object
+	 * @param \vinabb\web\controllers\acp\helper\setting_tasks_interface	$task_helper	Task helper
 	*/
 	public function __construct(
 		\phpbb\auth\auth $auth,
 		\vinabb\web\controllers\cache\service_interface $cache,
 		\phpbb\config\config $config,
 		\phpbb\config\db_text $config_text,
-		\phpbb\db\driver\driver_interface $db,
 		\phpbb\language\language $language,
 		\phpbb\log\log $log,
 		\phpbb\request\request $request,
 		\phpbb\template\template $template,
-		\phpbb\user $user
+		\phpbb\user $user,
+		\vinabb\web\controllers\acp\helper\setting_tasks_interface $task_helper
 	)
 	{
 		$this->auth = $auth;
 		$this->cache = $cache;
 		$this->config = $config;
 		$this->config_text = $config_text;
-		$this->db = $db;
 		$this->language = $language;
 		$this->log = $log;
 		$this->request = $request;
 		$this->template = $template;
 		$this->user = $user;
+		$this->task_helper = $task_helper;
 
 		$this->config_text_data = $this->cache->get_config_text();
 		$this->lang_data = $this->cache->get_lang_data();
@@ -166,7 +166,7 @@ class settings
 	{
 		return [
 			'ACP_CAT_MAINTENANCE'	=> [
-				'maintenance_mode'		=> ['type' => 'radio', 'type_data' => $this->maintenance_mode_type_data(), 'default' => 0, 'check' => 'method', 'check_data' => 'maintenance_mode_founder', 'task' => 'maintenance_mode_founder_task'],
+				'maintenance_mode'		=> ['type' => 'radio', 'type_data' => $this->task_helper->maintenance_mode_type_data(), 'default' => 0, 'check' => 'method', 'check_data' => 'maintenance_mode_founder', 'task' => 'maintenance_mode_founder_task'],
 				'maintenance_tpl'		=> ['type' => 'radio', 'default' => true],
 				'maintenance_time'		=> ['type' => 'int', 'explain' => true, 'append' => $this->language->lang('MINUTES'), 'default' => 0, 'task' => 'maintenance_time_task', 'unset' => true],
 				'maintenance_text'		=> ['type' => 'text_uni', 'explain' => true, 'default' => ''],
@@ -296,20 +296,20 @@ class settings
 	{
 		return [
 			'ACP_LANGUAGE'			=> [
-				'default_language'	=> ['type' => 'tpl', 'default' => $this->get_default_lang_name()],
-				'lang_switch'		=> ['type' => 'select', 'explain' => true, 'default' => $this->build_lang_list($this->config['vinabb_web_lang_switch'])],
+				'default_language'	=> ['type' => 'tpl', 'default' => $this->task_helper->get_default_lang_name()],
+				'lang_switch'		=> ['type' => 'select', 'explain' => true, 'default' => $this->task_helper->build_lang_list($this->config['vinabb_web_lang_switch'])],
 			],
 			'FORUMS'				=> [
-				'forum_id_vietnamese'				=> ['type' => 'select', 'default' => $this->build_forum_list($this->config['vinabb_web_forum_id_vietnamese'])],
-				'forum_id_vietnamese_support'		=> ['type' => 'select', 'default' => $this->build_forum_list($this->config['vinabb_web_forum_id_vietnamese_support'])],
-				'forum_id_vietnamese_ext'			=> ['type' => 'select', 'default' => $this->build_forum_list($this->config['vinabb_web_forum_id_vietnamese_ext'])],
-				'forum_id_vietnamese_style'			=> ['type' => 'select', 'default' => $this->build_forum_list($this->config['vinabb_web_forum_id_vietnamese_style'])],
-				'forum_id_vietnamese_tutorial'		=> ['type' => 'select', 'default' => $this->build_forum_list($this->config['vinabb_web_forum_id_vietnamese_tutorial'])],
-				'forum_id_vietnamese_discussion'	=> ['type' => 'select', 'default' => $this->build_forum_list($this->config['vinabb_web_forum_id_vietnamese_discussion'])],
-				'forum_id_english'					=> ['type' => 'select', 'default' => $this->build_forum_list($this->config['vinabb_web_forum_id_english'])],
-				'forum_id_english_support'			=> ['type' => 'select', 'default' => $this->build_forum_list($this->config['vinabb_web_forum_id_english_support'])],
-				'forum_id_english_tutorial'			=> ['type' => 'select', 'default' => $this->build_forum_list($this->config['vinabb_web_forum_id_english_tutorial'])],
-				'forum_id_english_discussion'		=> ['type' => 'select', 'default' => $this->build_forum_list($this->config['vinabb_web_forum_id_english_discussion'])]
+				'forum_id_vietnamese'				=> ['type' => 'select', 'default' => $this->task_helper->build_forum_list($this->config['vinabb_web_forum_id_vietnamese'])],
+				'forum_id_vietnamese_support'		=> ['type' => 'select', 'default' => $this->task_helper->build_forum_list($this->config['vinabb_web_forum_id_vietnamese_support'])],
+				'forum_id_vietnamese_ext'			=> ['type' => 'select', 'default' => $this->task_helper->build_forum_list($this->config['vinabb_web_forum_id_vietnamese_ext'])],
+				'forum_id_vietnamese_style'			=> ['type' => 'select', 'default' => $this->task_helper->build_forum_list($this->config['vinabb_web_forum_id_vietnamese_style'])],
+				'forum_id_vietnamese_tutorial'		=> ['type' => 'select', 'default' => $this->task_helper->build_forum_list($this->config['vinabb_web_forum_id_vietnamese_tutorial'])],
+				'forum_id_vietnamese_discussion'	=> ['type' => 'select', 'default' => $this->task_helper->build_forum_list($this->config['vinabb_web_forum_id_vietnamese_discussion'])],
+				'forum_id_english'					=> ['type' => 'select', 'default' => $this->task_helper->build_forum_list($this->config['vinabb_web_forum_id_english'])],
+				'forum_id_english_support'			=> ['type' => 'select', 'default' => $this->task_helper->build_forum_list($this->config['vinabb_web_forum_id_english_support'])],
+				'forum_id_english_tutorial'			=> ['type' => 'select', 'default' => $this->task_helper->build_forum_list($this->config['vinabb_web_forum_id_english_tutorial'])],
+				'forum_id_english_discussion'		=> ['type' => 'select', 'default' => $this->task_helper->build_forum_list($this->config['vinabb_web_forum_id_english_discussion'])]
 			],
 			'FOOTER_MANAGER_ROLE'	=> [
 				'manager_name'		=> ['type' => 'string_uni', 'default' => ''],
@@ -545,9 +545,9 @@ class settings
 		{
 			foreach ($this->tasks as $method_name => $value)
 			{
-				if (method_exists($this, $method_name))
+				if (method_exists($this->task_helper, $method_name))
 				{
-					$this->$method_name($value);
+					$this->task_helper->$method_name($value);
 				}
 			}
 		}
@@ -571,216 +571,5 @@ class settings
 			$this->config_text->set_array($this->data['config_text_data']);
 			$this->cache->clear_config_text();
 		}
-	}
-
-	/**
-	* Radio options for the config item 'maintenance_mode'
-	*
-	* @return array
-	*/
-	protected function maintenance_mode_type_data()
-	{
-		return [
-			constants::MAINTENANCE_MODE_NONE	=> $this->language->lang('MAINTENANCE_MODE_NONE'),
-			constants::MAINTENANCE_MODE_FOUNDER	=> $this->language->lang('MAINTENANCE_MODE_FOUNDER'),
-			constants::MAINTENANCE_MODE_ADMIN	=> $this->language->lang('MAINTENANCE_MODE_ADMIN'),
-			constants::MAINTENANCE_MODE_MOD		=> $this->language->lang('MAINTENANCE_MODE_MOD'),
-			constants::MAINTENANCE_MODE_USER	=> $this->language->lang('MAINTENANCE_MODE_USER')
-		];
-	}
-
-	/**
-	* Check only founders can set the founder-level maintenance mode
-	*
-	* @param int $value Input value
-	* @return bool
-	*/
-	protected function maintenance_mode_founder($value)
-	{
-		return !($value == constants::MAINTENANCE_MODE_FOUNDER && $this->user->data['user_type'] != USER_FOUNDER);
-	}
-
-	/**
-	* Kill out all normal administrators from the ACP
-	* keep only founder-level sessions
-	*
-	* @param int $value Input value
-	*/
-	protected function maintenance_mode_founder_task($value)
-	{
-		if ($value == constants::MAINTENANCE_MODE_FOUNDER)
-		{
-			$founder_user_ids = [];
-
-			$sql = 'SELECT user_id
-				FROM ' . USERS_TABLE . '
-				WHERE user_type = ' . USER_FOUNDER;
-			$result = $this->db->sql_query($sql);
-			$rows = $this->db->sql_fetchrowset($result);
-			$this->db->sql_freeresult($result);
-
-			foreach ($rows as $row)
-			{
-				$founder_user_ids[] = $row['user_id'];
-			}
-
-			if (sizeof($founder_user_ids))
-			{
-				$sql = 'UPDATE ' . SESSIONS_TABLE . '
-					SET session_admin = 0
-					WHERE session_admin = 1
-						AND ' . $this->db->sql_in_set('session_user_id', $founder_user_ids, true);
-				$this->db->sql_query($sql);
-			}
-		}
-	}
-
-	/**
-	* Convert the scheduled maintenance time from 'number of minutes' into 'UNIX timestamp'
-	*
-	* @param int $value Input value
-	*/
-	protected function maintenance_time_task($value)
-	{
-		$value = (int) $value;
-		$time = time() + ($value * 60);
-		$maintenance_time_reset = $this->request->variable('maintenance_time_reset', false);
-
-		if ($value || $maintenance_time_reset)
-		{
-			$this->config->set('vinabb_web_maintenance_time', $time);
-		}
-	}
-
-	/**
-	* Reset the stored newest phpBB version if the branch has changed
-	*
-	* @param string $value Input value
-	*/
-	protected function reset_phpbb_version_task($value)
-	{
-		if ($value != '' && $this->config['vinabb_web_check_phpbb_branch'] != '')
-		{
-			$this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'LOG_VINABB_CHANGE_CHECK_PHPBB_BRANCH', time(), [$this->config['vinabb_web_check_phpbb_branch'], $value]);
-		}
-
-		$this->config->set('vinabb_web_check_gc', 0, true);
-		$this->config->set('vinabb_web_check_phpbb_version', '');
-	}
-
-	/**
-	* Reset the stored newest phpBB legacy version if the branch has changed
-	*
-	* @param string $value Input value
-	*/
-	protected function reset_phpbb_legacy_version_task($value)
-	{
-		if ($value != '' && $this->config['vinabb_web_check_phpbb_legacy_branch'] != '')
-		{
-			$this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'LOG_VINABB_CHANGE_CHECK_PHPBB_LEGACY_BRANCH', time(), [$this->config['vinabb_web_check_phpbb_legacy_branch'], $value]);
-		}
-
-		$this->config->set('vinabb_web_check_gc', 0, true);
-		$this->config->set('vinabb_web_check_phpbb_legacy_version', '');
-	}
-
-	/**
-	* Reset the stored newest phpBB development version if the branch has changed
-	*
-	* @param string $value Input value
-	*/
-	protected function reset_phpbb_dev_version_task($value)
-	{
-		if ($value != '' && $this->config['vinabb_web_check_phpbb_dev_branch'] != '')
-		{
-			$this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'LOG_VINABB_CHANGE_CHECK_PHPBB_DEV_BRANCH', time(), [$this->config['vinabb_web_check_phpbb_dev_branch'], $value]);
-		}
-
-		$this->config->set('vinabb_web_check_gc', 0, true);
-		$this->config->set('vinabb_web_check_phpbb_dev_version', '');
-	}
-
-	/**
-	* Reset the stored newest PHP version if the branch has changed
-	*
-	* @param string $value Input value
-	*/
-	protected function reset_php_version_task($value)
-	{
-		if ($value != '' && $this->config['vinabb_web_check_php_branch'] != '')
-		{
-			$this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'LOG_VINABB_CHANGE_CHECK_PHP_BRANCH', time(), [$this->config['vinabb_web_check_php_branch'], $value]);
-		}
-
-		$this->config->set('vinabb_web_check_gc', 0, true);
-		$this->config->set('vinabb_web_check_php_version', '');
-	}
-
-	/**
-	* Reset the stored newest PHP legacy version if the branch has changed
-	*
-	* @param string $value Input value
-	*/
-	protected function reset_php_legacy_version_task($value)
-	{
-		if ($value != '' && $this->config['vinabb_web_check_php_legacy_branch'] != '')
-		{
-			$this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'LOG_VINABB_CHANGE_CHECK_PHP_LEGACY_BRANCH', time(), [$this->config['vinabb_web_check_php_legacy_branch'], $value]);
-		}
-
-		$this->config->set('vinabb_web_check_gc', 0, true);
-		$this->config->set('vinabb_web_check_php_legacy_version', '');
-	}
-
-	/**
-	* Get default language name
-	*
-	* @return string
-	*/
-	protected function get_default_lang_name()
-	{
-		return $this->lang_data[$this->config['default_lang']]['english_name'] . ' (' . $this->lang_data[$this->config['default_lang']]['local_name'] . ')';
-	}
-
-	/**
-	* Select an extra language to switch
-	*
-	* @param string $selected_lang 2-letter language ISO code
-	* @return string HTML code
-	*/
-	protected function build_lang_list($selected_lang)
-	{
-		$lang_switch_options = '<option value=""' . (($selected_lang == '') ? ' selected' : '') . '>' . $this->language->lang('SELECT_LANGUAGE') . '</option>';
-
-		foreach ($this->lang_data as $lang_iso => $data)
-		{
-			if ($lang_iso != $this->config['default_lang'])
-			{
-				$lang_switch_options .= '<option value="' . $lang_iso . '"' . (($selected_lang == $lang_iso) ? ' selected' : '') . '>' . $data['english_name'] . ' (' . $data['local_name'] . ')</option>';
-			}
-		}
-
-		return $lang_switch_options;
-	}
-
-	/**
-	* Select categories (not postable forums)
-	*
-	* @param int $selected_forum Selected forum ID
-	* @return string HTML code
-	*/
-	protected function build_forum_list($selected_forum)
-	{
-		$forum_options = '<option value=""' . (($selected_forum == 0) ? ' selected' : '') . '>' . $this->language->lang('SELECT_FORUM') . '</option>';
-
-		foreach ($this->cache->get_forum_data() as $forum_id => $data)
-		{
-			if ($data['type'] == FORUM_CAT)
-			{
-				$forum_options .= '<option value="' . $forum_id . '"' . (($selected_forum == $forum_id) ? ' selected' : '') . '>' . $data['name'] . '</option>';
-			}
-		}
-
-		return $forum_options;
 	}
 }
