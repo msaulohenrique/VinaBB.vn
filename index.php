acfe411983ddae30acc4426db04b6574aed3d2cf
Khoa Nguyen <nedkavn@gmail.com>
2016-11-24 20:19:53 +0700

    Remove PHP events on index.php

--- index.php
+++ index.php
@@ -24,21 +24,8 @@ if ($phpbb_extension_manager->is_enabled('vinabb/web'))
 	// Include the portal controller
 	$phpbb_container->get('vinabb.web.portal')->index();
 
-	// Index page title
-	$page_title = $phpbb_container->get('language')->lang('VINABB');
-
-	/**
-	* You can use this event to modify the page title and load data for the index
-	*
-	* @event core.index_modify_page_title
-	* @var	string	page_title		Title of the index page
-	* @since 3.1.0-a1
-	*/
-	$vars = ['page_title'];
-	extract($phpbb_dispatcher->trigger_event('core.index_modify_page_title', compact($vars)));
-
 	// Output the page
-	page_header($page_title, true);
+	page_header($phpbb_container->get('language')->lang('VINABB'), true);
 
 	$template->set_filenames(['body' => '@vinabb_web/portal.html']);
 
