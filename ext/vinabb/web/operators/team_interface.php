698381a5c316d597f7a14c68f60fa5ddfc30beb5
Khoa Nguyen <nedkavn@gmail.com>
2016-11-26 05:32:42 +0700

    Add interface for the team operators class

---
+++ ext/vinabb/web/operators/team_interface.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\web\operators;
+
+/**
+* Interface for a set of teams
+*/
+interface team_interface
+{
+	/**
+	* Get all teams
+	*
+	* @return array
+	*/
+	public function get_teams();
+
+	/**
+	* Add a team
+	*
+	* @param \vinabb\web\entities\team_interface $entity Team entity
+	* @return \vinabb\web\entities\team_interface
+	*/
+	public function add_team(\vinabb\web\entities\team_interface $entity);
+
+	/**
+	* Delete a team
+	*
+	* @param int $id Team ID
+	* @return bool True if row was deleted, false otherwise
+	* @throws \vinabb\web\exceptions\out_of_bounds
+	*/
+	public function delete_team($id);
+}
