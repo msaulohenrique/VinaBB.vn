7d4a6d859ca43d5d9ce8e613d3d5abddac91676a
Khoa Nguyen <nedkavn@gmail.com>
2016-11-26 05:05:09 +0700

    Add the censor word entity class

---
+++ ext/vinabb/web/entities/censor_word.php
@@ -0,0 +1,248 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\web\entities;
+
+use vinabb\web\includes\constants;
+
+/**
+* Entity for a single censor word
+*/
+class censor_word implements censor_word_interface
+{
+	/** @var \phpbb\db\driver\driver_interface */
+	protected $db;
+
+	/** @var array */
+	protected $data;
+
+	/**
+	* Constructor
+	*
+	* @param \phpbb\db\driver\driver_interface $db Database object
+	*/
+	public function __construct(\phpbb\db\driver\driver_interface $db)
+	{
+		$this->db = $db;
+	}
+
+	/**
+	* Data for this entity
+	*
+	* @return array
+	*/
+	protected function prepare_data()
+	{
+		return [
+			'word_id'		=> 'integer',
+			'word'			=> 'string',
+			'replacement'	=> 'string'
+		];
+	}
+
+	/**
+	* Load the data from the database for an entity
+	*
+	* @param int					$id		Word ID
+	* @return censor_word_interface	$this	Object for chaining calls: load()->set()->save()
+	* @throws \vinabb\web\exceptions\out_of_bounds
+	*/
+	public function load($id)
+	{
+		$sql = 'SELECT *
+			FROM ' . WORDS_TABLE . '
+			WHERE word_id = ' . (int) $id;
+		$result = $this->db->sql_query($sql);
+		$this->data = $this->db->sql_fetchrow($result);
+		$this->db->sql_freeresult($result);
+
+		// The entity does not exist
+		if ($this->data === false)
+		{
+			throw new \vinabb\web\exceptions\out_of_bounds('word_id');
+		}
+
+		return $this;
+	}
+
+	/**
+	* Import data for an entity
+	*
+	* Used when the data is already loaded externally.
+	* Any existing data on this entity is over-written.
+	* All data is validated and an exception is thrown if any data is invalid.
+	*
+	* @param array					$data	Data array from the database
+	* @return censor_word_interface	$this	Object for chaining calls: load()->set()->save()
+	* @throws \vinabb\web\exceptions\invalid_argument
+	*/
+	public function import($data)
+	{
+		// Clear out any saved data
+		$this->data = [];
+
+		// Go through the basic fields and set them to our data array
+		foreach ($this->prepare_data() as $field => $type)
+		{
+			// The data wasn't sent to us
+			if (!isset($data[$field]))
+			{
+				throw new \vinabb\web\exceptions\invalid_argument([$field, 'EMPTY']);
+			}
+
+			// settype() passes values by reference
+			$value = $data[$field];
+
+			// We're using settype() to enforce data types
+			settype($value, $type);
+
+			$this->data[$field] = $value;
+		}
+
+		return $this;
+	}
+
+	/**
+	* Insert the entity for the first time
+	*
+	* Will throw an exception if the entity was already inserted (call save() instead)
+	*
+	* @return censor_word_interface $this Object for chaining calls: load()->set()->save()
+	* @throws \vinabb\web\exceptions\out_of_bounds
+	*/
+	public function insert()
+	{
+		// The entity already exists
+		if (!empty($this->data['word_id']))
+		{
+			throw new \vinabb\web\exceptions\out_of_bounds('word_id');
+		}
+
+		// Make extra sure there is no ID set
+		unset($this->data['word_id']);
+
+		$sql = 'INSERT INTO ' . WORDS_TABLE . ' ' . $this->db->sql_build_array('INSERT', $this->data);
+		$this->db->sql_query($sql);
+
+		// Set the ID using the ID created by the SQL INSERT
+		$this->data['word_id'] = (int) $this->db->sql_nextid();
+
+		return $this;
+	}
+
+	/**
+	* Save the current settings to the database
+	*
+	* This must be called before closing or any changes will not be saved!
+	* If adding an entity (saving for the first time), you must call insert() or an exception will be thrown
+	*
+	* @return censor_word_interface $this Object for chaining calls: load()->set()->save()
+	* @throws \vinabb\web\exceptions\out_of_bounds
+	*/
+	public function save()
+	{
+		// The entity does not exist
+		if (empty($this->data['word_id']))
+		{
+			throw new \vinabb\web\exceptions\out_of_bounds('word_id');
+		}
+
+		// Copy the data array, filtering out the ID
+		// so we do not attempt to update the row's identity column.
+		$sql_array = array_diff_key($this->data, ['word_id' => null]);
+
+		$sql = 'UPDATE ' . WORDS_TABLE . '
+			SET ' . $this->db->sql_build_array('UPDATE', $sql_array) . '
+			WHERE word_id = ' . $this->get_id();
+		$this->db->sql_query($sql);
+
+		return $this;
+	}
+
+	/**
+	* Get the word_id
+	*
+	* @return int
+	*/
+	public function get_id()
+	{
+		return isset($this->data['word_id']) ? (int) $this->data['word_id'] : 0;
+	}
+
+	/**
+	* Get the censor word
+	*
+	* @return string
+	*/
+	public function get_word()
+	{
+		return isset($this->data['word']) ? (string) $this->data['word'] : '';
+	}
+
+	/**
+	* Set the censor word
+	*
+	* @param string					$text	Censor word
+	* @return censor_word_interface	$this	Object for chaining calls: load()->set()->save()
+	* @throws \vinabb\web\exceptions\unexpected_value
+	*/
+	public function set_word($text)
+	{
+		$text = (string) $text;
+
+		// This is a required field
+		if ($text == '')
+		{
+			throw new \vinabb\web\exceptions\unexpected_value(['word', 'EMPTY']);
+		}
+
+		// Check the max length
+		if (truncate_string($text, constants::MAX_PAGE_NAME) != $text)
+		{
+			throw new \vinabb\web\exceptions\unexpected_value(['word', 'TOO_LONG']);
+		}
+
+		// Set the value on our data array
+		$this->data['word'] = $text;
+
+		return $this;
+	}
+
+	/**
+	* Get the replacement word
+	*
+	* @return string
+	*/
+	public function get_replacement()
+	{
+		return isset($this->data['replacement']) ? (string) $this->data['replacement'] : '';
+	}
+
+	/**
+	* Set the replacement word
+	*
+	* @param string					$text	Replacement word
+	* @return censor_word_interface	$this	Object for chaining calls: load()->set()->save()
+	* @throws \vinabb\web\exceptions\unexpected_value
+	*/
+	public function set_replacement($text)
+	{
+		$text = (string) $text;
+
+		// Check the max length
+		if (truncate_string($text, constants::MAX_PAGE_NAME) != $text)
+		{
+			throw new \vinabb\web\exceptions\unexpected_value(['replacement', 'TOO_LONG']);
+		}
+
+		// Set the value on our data array
+		$this->data['replacement'] = $text;
+
+		return $this;
+	}
+}
