ac83ede6c6b1b73bb186826c529da8007944be3f
Khoa Nguyen <nedkavn@gmail.com>
2016-11-26 04:50:17 +0700

    Add interface for the rank operators class

---
+++ ext/vinabb/web/operators/rank_interface.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\web\operators;
+
+/**
+* Interface for a set of ranks
+*/
+interface rank_interface
+{
+	/**
+	* Get all ranks
+	*
+	* @return array
+	*/
+	public function get_ranks();
+
+	/**
+	* Add a rank
+	*
+	* @param \vinabb\web\entities\rank_interface $entity Rank entity
+	* @return \vinabb\web\entities\rank_interface
+	*/
+	public function add_rank(\vinabb\web\entities\rank_interface $entity);
+
+	/**
+	* Delete a rank
+	*
+	* @param int $id Rank ID
+	* @return bool True if row was deleted, false otherwise
+	* @throws \vinabb\web\exceptions\out_of_bounds
+	*/
+	public function delete_rank($id);
+}
