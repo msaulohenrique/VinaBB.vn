c4f8479c6fb01f06b63ae51636eedb4d7e59a1c6
Khoa Nguyen <nedkavn@gmail.com>
2016-11-26 08:36:59 +0700

    Add interface for the poll option operators class

---
+++ ext/vinabb/web/operators/poll_option_interface.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* This file is part of the VinaBB.vn package.
+*
+* @copyright (c) VinaBB <vinabb.vn>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace vinabb\web\operators;
+
+/**
+* Interface for a set of poll options
+*/
+interface poll_option_interface
+{
+	/**
+	* Get all options
+	*
+	* @param int $topic_id Topic ID
+	* @return array
+	* @throws \vinabb\web\exceptions\out_of_bounds
+	*/
+	public function get_options($topic_id = 0);
+
+	/**
+	* Add an option
+	*
+	* @param \vinabb\web\entities\poll_option_interface $entity Poll option entity
+	* @return \vinabb\web\entities\poll_option_interface
+	*/
+	public function add_option(\vinabb\web\entities\poll_option_interface $entity);
+
+	/**
+	* Delete an option
+	*
+	* @param int $id Draft ID
+	* @return bool True if row was deleted, false otherwise
+	* @throws \vinabb\web\exceptions\out_of_bounds
+	*/
+	public function delete_option($id);
+}
