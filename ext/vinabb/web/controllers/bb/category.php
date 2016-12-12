8cae97cc29ff485b2a4c987516674585321f6bc9
Khoa Nguyen <nedkavn@gmail.com>
2016-12-12 19:28:10 +0700

    phpBB Resource: Add the method list_bb_items()

--- ext/vinabb/web/controllers/bb/category.php
+++ ext/vinabb/web/controllers/bb/category.php
@@ -8,6 +8,7 @@
 
 namespace vinabb\web\controllers\bb;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use vinabb\web\includes\constants;
 
 /**
@@ -17,6 +18,9 @@ class category
 {
 	/** @var \vinabb\web\controllers\cache\service_interface $cache */
 	protected $cache;
+
+	/** @var ContainerInterface $container */
+	protected $container;
 
 	/** @var \phpbb\language\language $language */
 	protected $language;
@@ -37,6 +41,7 @@ class category
 	* Constructor
 	*
 	* @param \vinabb\web\controllers\cache\service_interface	$cache		Cache service
+	* @param ContainerInterface									$container	Service container
 	* @param \phpbb\language\language							$language	Language object
 	* @param \phpbb\user										$user		User object
 	* @param \phpbb\controller\helper							$helper		Controller helper
@@ -44,6 +49,7 @@ class category
 	*/
 	public function __construct(
 		\vinabb\web\controllers\cache\service_interface $cache,
+		ContainerInterface $container,
 		\phpbb\language\language $language,
 		\phpbb\user $user,
 		\phpbb\controller\helper $helper,
@@ -51,6 +57,7 @@ class category
 	)
 	{
 		$this->cache = $cache;
+		$this->container = $container;
 		$this->language = $language;
 		$this->user = $user;
 		$this->helper = $helper;
@@ -82,5 +89,48 @@ class category
 		$this->ext_helper->set_breadcrumb($cat_name);
 
 		trigger_error($this->language->lang('NO_BB_' . strtoupper($type) . 'S'));
+	}
+
+	/**
+	* List phpBB resource items with pagination
+	*
+	* @param int	$bb_type	phpBB resource type in constant value
+	* @param int	$cat_id		Category ID
+	* @param array	$items		Array of items
+	* @param int	$item_count	Number of items
+	* @param int	$limit		Items per page
+	* @param int	$offset		Position of the start
+	*
+	* @return int Position of the start
+	*/
+	public function list_bb_items($bb_type, $cat_id, &$items, &$item_count, $limit = 0, $offset = 0)
+	{
+		$operators = $this->container->get('vinabb.web.operators.bb_item');
+		$item_count = $operators->count_items($bb_type, $cat_id);
+
+		if ($item_count == 0)
+		{
+			return 0;
+		}
+
+		if ($offset >= $item_count)
+		{
+			$offset = ($offset - $limit < 0) ? 0 : $offset - $limit;
+		}
+
+		/** @var \vinabb\web\entities\bb_item_interface $entity */
+		foreach ($operators->list_items($bb_type, $cat_id, 'item_updated DESC', $limit, $offset) as $entity)
+		{
+			$items[] = [
+				'id'		=> $entity->get_id(),
+				'name'		=> $entity->get_name(),
+				'varname'	=> $entity->get_varname(),
+				'price'		=> $entity->get_price(),
+				'added'		=> $entity->get_added(),
+				'updated'	=> $entity->get_updated()
+			];
+		}
+
+		return $offset;
 	}
 }
