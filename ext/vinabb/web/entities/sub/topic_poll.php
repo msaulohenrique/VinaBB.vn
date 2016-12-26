961fe158107b7b01dd18da8764e5582bd9f3362e
Khoa Nguyen <nedkavn@gmail.com>
2016-12-26 10:44:03 +0700

    Add new set methods to the topic_poll entity

--- ext/vinabb/web/entities/sub/topic_poll.php
+++ ext/vinabb/web/entities/sub/topic_poll.php
@@ -62,6 +62,20 @@ class topic_poll
 	}
 
 	/**
+	* Set the poll's staring time
+	*
+	* @param int			$value	UNIX timestamp
+	* @return topic_poll	$this	Object for chaining calls: load()->set()->save()
+	*/
+	public function set_poll_start($value)
+	{
+		// Set the value on our data array
+		$this->data['poll_start'] = (int) $value;
+
+		return $this;
+	}
+
+	/**
 	* Get the poll's ending time
 	*
 	* @return int
@@ -69,6 +83,20 @@ class topic_poll
 	public function get_poll_length()
 	{
 		return isset($this->data['poll_length']) ? (int) $this->data['poll_length'] : 0;
+	}
+
+	/**
+	* Set the poll's ending time
+	*
+	* @param int			$value	UNIX timestamp
+	* @return topic_poll	$this	Object for chaining calls: load()->set()->save()
+	*/
+	public function set_poll_length($value)
+	{
+		// Set the value on our data array
+		$this->data['poll_length'] = (int) $value;
+
+		return $this;
 	}
 
 	/**
@@ -82,6 +110,20 @@ class topic_poll
 	}
 
 	/**
+	* Set the maximum options of poll
+	*
+	* @param int			$value	Number of options
+	* @return topic_poll	$this	Object for chaining calls: load()->set()->save()
+	*/
+	public function set_poll_max_options($value)
+	{
+		// Set the value on our data array
+		$this->data['poll_max_options'] = (int) $value;
+
+		return $this;
+	}
+
+	/**
 	* Get the poll's last voting time
 	*
 	* @return int
@@ -92,12 +134,40 @@ class topic_poll
 	}
 
 	/**
-	* Does the poll allow to change voted options?
+	* Set the poll's last voting time
+	*
+	* @param int			$value	UNIX timestamp
+	* @return topic_poll	$this	Object for chaining calls: load()->set()->save()
+	*/
+	public function set_poll_last_vote($value)
+	{
+		// Set the value on our data array
+		$this->data['poll_last_vote'] = (int) $value;
+
+		return $this;
+	}
+
+	/**
+	* Get the poll option: Allow to change voted options?
 	*
 	* @return bool
 	*/
 	public function get_poll_vote_change()
 	{
 		return isset($this->data['poll_vote_change']) ? (bool) $this->data['poll_vote_change'] : false;
+	}
+
+	/**
+	* Set the poll option: Allow to change voted options?
+	*
+	* @param int			$value	true: allow; false: disallow
+	* @return topic_poll	$this	Object for chaining calls: load()->set()->save()
+	*/
+	public function set_poll_vote_change($value)
+	{
+		// Set the value on our data array
+		$this->data['poll_vote_change'] = (bool) $value;
+
+		return $this;
 	}
 }
