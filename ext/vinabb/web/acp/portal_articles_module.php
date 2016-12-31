75dccc49c9b578eee916db6b6f701927655ea98a
Khoa Nguyen <nedkavn@gmail.com>
2016-12-31 16:39:10 +0700

    Scrutinizer: The method display_articles() does not seem to exist

--- ext/vinabb/web/acp/portal_articles_module.php
+++ ext/vinabb/web/acp/portal_articles_module.php
@@ -13,7 +13,7 @@ namespace vinabb\web\acp;
 */
 class portal_articles_module
 {
-	/** @var \vinabb\web\controllers\acp\portal_comments_interface $controller */
+	/** @var \vinabb\web\controllers\acp\portal_articles_interface $controller */
 	protected $controller;
 
 	/** @var \phpbb\language\language $language */
