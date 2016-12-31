983328c59263b2e26065228fe1914de3431ff9d6
Khoa Nguyen <nedkavn@gmail.com>
2016-12-31 16:38:26 +0700

    Scrutinizer: The method display_menus() does not seem to exist

--- ext/vinabb/web/acp/menus_module.php
+++ ext/vinabb/web/acp/menus_module.php
@@ -13,7 +13,7 @@ namespace vinabb\web\acp;
 */
 class menus_module
 {
-	/** @var \vinabb\web\controllers\acp\portal_comments_interface $controller */
+	/** @var \vinabb\web\controllers\acp\menus_interface $controller */
 	protected $controller;
 
 	/** @var \phpbb\language\language $language */
