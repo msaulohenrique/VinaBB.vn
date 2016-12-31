905286b9c9531bb0ffc55910b18bb9cc20f92995
Khoa Nguyen <nedkavn@gmail.com>
2016-12-31 16:37:51 +0700

    Scrutinizer: The method select_lang() does not seem to exist

--- ext/vinabb/web/acp/headlines_module.php
+++ ext/vinabb/web/acp/headlines_module.php
@@ -13,7 +13,7 @@ namespace vinabb\web\acp;
 */
 class headlines_module
 {
-	/** @var \vinabb\web\controllers\acp\portal_comments_interface $controller */
+	/** @var \vinabb\web\controllers\acp\headlines_interface $controller */
 	protected $controller;
 
 	/** @var \phpbb\language\language $language */
