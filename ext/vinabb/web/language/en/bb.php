3b852803b3ba3f4a250862cdb3b819957061cf18
Khoa Nguyen <nedkavn@gmail.com>
2016-12-31 07:27:47 +0700

    phpBB Resource: Add new lang files for front-end

--- ext/vinabb/web/language/en/bb.php
+++ ext/vinabb/web/language/en/bb.php
@@ -29,4 +29,64 @@ if (empty($lang) || !is_array($lang))
 * Translators: Do not need to translate these language strings ;)
 */
 $lang = array_merge($lang, [
+	'ACP_STYLE_DETAILS'	=> 'Style information',
+	'ACP_STYLE_NAME'	=> 'Style name',
+	'ACP_STYLE_VERSION'	=> 'Style version',
+
+	'EXT_DETAILS'				=> 'Extension information',
+	'EXT_NAME'					=> 'Extension name',
+	'EXT_PROPERTIES'			=> [
+		'ACP_STYLE'			=> 'ACP Styles',
+		'CONSOLE'			=> 'Console Commands',
+		'CRON'				=> 'Cron Tasks',
+		'DB_DATA'			=> 'Table Data',
+		'DB_SCHEMA'			=> 'Database Schema',
+		'EVENT'				=> 'PHP Events',
+		'EVENT_ACP_STYLE'	=> 'ACP Template Events',
+		'EVENT_STYLE'		=> 'Template Events',
+		'EXT_PHP'			=> 'Extension Installer',
+		'LANG'				=> 'Language Files',
+		'MODULE_ACP'		=> 'ACP Modules',
+		'MODULE_MCP'		=> 'MCP Modules',
+		'MODULE_UCP'		=> 'UCP Modules',
+		'NOTIFICATION'		=> 'Notifications',
+		'STYLE'				=> 'Styles'
+	],
+	'EXT_PROPERTIES_EXPLAIN'	=> [
+		'ACP_STYLE'			=> 'Add new HTML/CSS/JS files to the ACP style.',
+		'CONSOLE'			=> 'Add new console commands.',
+		'CRON'				=> 'Add new cron tasks.',
+		'DB_DATA'			=> 'Insert, update, delete rows in the database tables and modify config items.',
+		'DB_SCHEMA'			=> 'Create, alter, drop tables, columns and indexes in the database.',
+		'EVENT'				=> 'Modify PHP code via events.',
+		'EVENT_ACP_STYLE'	=> 'Insert HTML/CSS/JS code into ACP template files via events.',
+		'EVENT_STYLE'		=> 'Insert HTML/CSS/JS code into template files via events.',
+		'EXT_PHP'			=> 'Execute some actions while installing or uninstalling the extension.',
+		'LANG'				=> 'Add new language files.',
+		'MODULE_ACP'		=> 'Add new ACP modules.',
+		'MODULE_MCP'		=> 'Add new MCP modules.',
+		'MODULE_UCP'		=> 'Add new UCP modules.',
+		'NOTIFICATION'		=> 'Add new notification types.',
+		'STYLE'				=> 'Add new HTML/CSS/JS files to your styles.'
+	],
+	'EXT_VERSION'				=> 'Extension version',
+
+	'LANG_DETAILS'	=> 'Language package information',
+	'LANG_NAME'		=> 'Language package name',
+	'LANG_VERSION'	=> 'Language package version',
+
+	'STYLE_DETAILS'		=> 'Style information',
+	'STYLE_NAME'		=> 'Style name',
+	'STYLE_PROPERTIES'	=> [
+		'BOOTSTRAP'		=> 'Bootstrap',
+		'PRESETS'		=> 'Presets',
+		'PRESETS_AIO'	=> 'All-in-one presets',
+		'RESPONSIVE'	=> 'Responsive',
+		'SOURCE'		=> 'Source files'
+	],
+	'STYLE_VERSION'		=> 'Style version',
+
+	'TOOL_DETAILS'	=> 'Tool information',
+	'TOOL_NAME'		=> 'Tool name',
+	'TOOL_VERSION'	=> 'Tool version'
 ]);
--- ext/vinabb/web/language/vi/bb.php
+++ ext/vinabb/web/language/vi/bb.php
@@ -29,4 +29,64 @@ if (empty($lang) || !is_array($lang))
 * Translators: Do not need to translate these language strings ;)
 */
 $lang = array_merge($lang, [
+	'ACP_STYLE_DETAILS'	=> 'Thông tin giao diện',
+	'ACP_STYLE_NAME'	=> 'Tên giao diện',
+	'ACP_STYLE_VERSION'	=> 'Phiên bản giao diện',
+
+	'EXT_DETAILS'				=> 'Thông tin gói mở rộng',
+	'EXT_NAME'					=> 'Tên gói mở rộng',
+	'EXT_PROPERTIES'			=> [
+		'ACP_STYLE'			=> 'Giao diện quản trị',
+		'CONSOLE'			=> 'Dòng lệnh',
+		'CRON'				=> 'Thao tác hẹn giờ',
+		'DB_DATA'			=> 'Thay đổi dữ liệu',
+		'DB_SCHEMA'			=> 'Thay đổi cấu trúc dữ liệu',
+		'EVENT'				=> 'Sự kiện PHP',
+		'EVENT_ACP_STYLE'	=> 'Sự kiện giao diện quản trị',
+		'EVENT_STYLE'		=> 'Sự kiện giao diện',
+		'EXT_PHP'			=> 'Trình cài đặt gói mở rộng',
+		'LANG'				=> 'Tập tin ngôn ngữ',
+		'MODULE_ACP'		=> 'Gói chức năng quản trị',
+		'MODULE_MCP'		=> 'Gói chức năng điều hành',
+		'MODULE_UCP'		=> 'Gói chức năng người dùng',
+		'NOTIFICATION'		=> 'Thông báo',
+		'STYLE'				=> 'Giao diện'
+	],
+	'EXT_PROPERTIES_EXPLAIN'	=> [
+		'ACP_STYLE'			=> 'Thêm các tập tin HTML/CSS/JS mới vào giao diện quản trị.',
+		'CONSOLE'			=> 'Thêm các dòng lệnh mới.',
+		'CRON'				=> 'Thêm các thao tác hẹn giờ mới.',
+		'DB_DATA'			=> 'Chèn, cập nhật, xóa dòng trong cơ sở dữ liệu và sửa mục cấu hình.',
+		'DB_SCHEMA'			=> 'Tạo, sửa, xóa bảng, cột và chỉ mục trong cơ sở dữ liệu.',
+		'EVENT'				=> 'Sửa mã PHP thông qua các sự kiện.',
+		'EVENT_ACP_STYLE'	=> 'Chèn mã HTML/CSS/JS vào tập tin khuôn mẫu quản trị thông qua các sự kiện.',
+		'EVENT_STYLE'		=> 'Chèn mã HTML/CSS/JS vào tập tin khuôn mẫu thông qua các sự kiện.',
+		'EXT_PHP'			=> 'Thực thi một vài thao tác trong quá trình cài đặt hay gỡ bỏ gói mở rộng.',
+		'LANG'				=> 'Thêm các tập tin ngôn ngữ mới.',
+		'MODULE_ACP'		=> 'Thêm các gói chức năng quản trị mới.',
+		'MODULE_MCP'		=> 'Thêm các gói chức năng điều hành mới.',
+		'MODULE_UCP'		=> 'Thêm các gói chức năng người dùng mới.',
+		'NOTIFICATION'		=> 'Thêm các loại thông báo tương tác mới.',
+		'STYLE'				=> 'Thêm các tập tin HTML/CSS/JS mới vào giao diện.'
+	],
+	'EXT_VERSION'				=> 'Phiên bản gói mở rộng',
+
+	'LANG_DETAILS'	=> 'Thông tin gói ngôn ngữ',
+	'LANG_NAME'		=> 'Tên gói ngôn ngữ',
+	'LANG_VERSION'	=> 'Phiên bản gói ngôn ngữ',
+
+	'STYLE_DETAILS'		=> 'Thông tin giao diện',
+	'STYLE_NAME'		=> 'Tên giao diện',
+	'STYLE_PROPERTIES'	=> [
+		'BOOTSTRAP'		=> 'Bootstrap',
+		'PRESETS'		=> 'Số bản màu',
+		'PRESETS_AIO'	=> 'Tất cả bản màu trong một',
+		'RESPONSIVE'	=> 'Hỗ trợ đa thiết bị',
+		'SOURCE'		=> 'Hình thiết kế gốc'
+	],
+	'STYLE_VERSION'		=> 'Phiên bản giao diện',
+
+	'TOOL_DETAILS'	=> 'Thông tin công cụ',
+	'TOOL_NAME'		=> 'Tên công cụ',
+	'TOOL_VERSION'	=> 'Phiên bản công cụ'
 ]);
