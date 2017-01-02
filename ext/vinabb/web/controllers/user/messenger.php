2fe520dd268cd3a63bbb0b884986efefe8eac963
Khoa Nguyen <nedkavn@gmail.com>
2017-01-02 22:02:03 +0700

    Simplify the User/Messenger class (Jabber)

--- ext/vinabb/web/controllers/user/messenger.php
+++ ext/vinabb/web/controllers/user/messenger.php
@@ -13,49 +13,52 @@ namespace vinabb\web\controllers\user;
 */
 class messenger implements messenger_interface
 {
-	/** @var \phpbb\auth\auth */
+	/** @var \phpbb\auth\auth $auth */
 	protected $auth;
 
-	/** @var \phpbb\config\config */
+	/** @var \phpbb\config\config $config */
 	protected $config;
 
-	/** @var \phpbb\db\driver\driver_interface */
+	/** @var \phpbb\db\driver\driver_interface $db */
 	protected $db;
 
-	/** @var \phpbb\language\language */
+	/** @var \phpbb\language\language $language */
 	protected $language;
 
-	/** @var \phpbb\request\request */
+	/** @var \phpbb\request\request $request */
 	protected $request;
 
-	/** @var \phpbb\template\template */
+	/** @var \phpbb\template\template $template */
 	protected $template;
 
-	/** @var \phpbb\user */
+	/** @var \phpbb\user $user */
 	protected $user;
 
-	/** @var \phpbb\controller\helper */
+	/** @var \phpbb\controller\helper $helper */
 	protected $helper;
 
-	/** @var string */
+	/** @var string $root_path */
 	protected $root_path;
 
-	/** @var string */
+	/** @var string $php_ext */
 	protected $php_ext;
+
+	/** @var array $recipient_data */
+	protected $recipient_data;
 
 	/**
 	* Constructor
 	*
-	* @param \phpbb\auth\auth $auth
-	* @param \phpbb\config\config $config
-	* @param \phpbb\db\driver\driver_interface $db
-	* @param \phpbb\language\language $language
-	* @param \phpbb\request\request $request
-	* @param \phpbb\template\template $template
-	* @param \phpbb\user $user
-	* @param \phpbb\controller\helper $helper
-	* @param string $root_path
-	* @param string $php_ext
+	* @param \phpbb\auth\auth					$auth		Authentication object
+	* @param \phpbb\config\config				$config		Config object
+	* @param \phpbb\db\driver\driver_interface	$db			Database object
+	* @param \phpbb\language\language			$language	Language object
+	* @param \phpbb\request\request				$request	Request object
+	* @param \phpbb\template\template			$template	Template object
+	* @param \phpbb\user						$user		User object
+	* @param \phpbb\controller\helper			$helper		Controller helper
+	* @param string								$root_path	phpBB root path
+	* @param string								$php_ext	PHP file extension
 	*/
 	public function __construct(
 		\phpbb\auth\auth $auth,
@@ -100,106 +103,104 @@ class messenger implements messenger_interface
 			trigger_error('NOT_AUTHORISED');
 		}
 
-		$presence_img = '';
+		$s_select = (extension_loaded('xml') && $this->config['jab_enable']) ? 'S_SEND_JABBER' : 'S_NO_SEND_JABBER';
+		$s_action = $this->helper->route('vinabb_web_user_messenger_route', ['action' => $action, 'user_id' => $user_id]);
 
-		switch ($action)
-		{
-			case 'jabber':
-				$lang = 'JABBER';
-				$sql_field = 'user_jabber';
-				$s_select = (extension_loaded('xml') && $this->config['jab_enable']) ? 'S_SEND_JABBER' : 'S_NO_SEND_JABBER';
-				$s_action = $this->helper->route('vinabb_web_user_messenger_route', ['action' => $action, 'user_id' => $user_id]);
-			break;
-
-			default:
-				trigger_error('NO_MODE');
-			break;
-		}
-
-		// Grab relevant data
-		$sql = "SELECT user_id, username, user_email, user_lang, $sql_field
-			FROM " . USERS_TABLE . "
-			WHERE user_id = $user_id
-				AND " . $this->db->sql_in_set('user_type', [USER_NORMAL, USER_FOUNDER]);
-		$result = $this->db->sql_query($sql);
-		$row = $this->db->sql_fetchrow($result);
-		$this->db->sql_freeresult($result);
-
-		if (!$row)
-		{
-			trigger_error('NO_USER');
-		}
-		else if (empty($row[$sql_field]))
-		{
-			trigger_error('IM_NO_DATA');
-		}
+		$this->get_recipient_data($user_id);
 
 		// Post data grab actions
-		switch ($action)
+		add_form_key('memberlist_messaging');
+
+		if ($this->request->is_set_post('submit') && extension_loaded('xml') && $this->config['jab_enable'])
 		{
-			case 'jabber':
-				add_form_key('memberlist_messaging');
+			if (check_form_key('memberlist_messaging'))
+			{
+				$this->send_message();
 
-				if ($this->request->is_set_post('submit') && extension_loaded('xml') && $this->config['jab_enable'])
-				{
-					if (check_form_key('memberlist_messaging'))
-					{
-						if (!class_exists('messenger'))
-						{
-							include "{$this->root_path}includes/functions_messenger.{$this->php_ext}";
-						}
-
-						$subject = $this->language->lang('IM_JABBER_SUBJECT', $this->user->data['username'], $this->config['server_name']);
-						$message = $this->request->variable('message', '', true);
-
-						if (empty($message))
-						{
-							trigger_error('EMPTY_MESSAGE_IM');
-						}
-
-						$messenger = new \messenger(false);
-						$messenger->template('profile_send_im', $row['user_lang']);
-						$messenger->subject(htmlspecialchars_decode($subject));
-						$messenger->replyto($this->user->data['user_email']);
-						$messenger->set_addresses($row);
-
-						$messenger->assign_vars([
-							'BOARD_CONTACT'	=> phpbb_get_board_contact($this->config, $this->php_ext),
-							'FROM_USERNAME'	=> htmlspecialchars_decode($this->user->data['username']),
-							'TO_USERNAME'	=> htmlspecialchars_decode($row['username']),
-							'MESSAGE'		=> htmlspecialchars_decode($message)
-						]);
-
-						$messenger->send(NOTIFY_IM);
-
-						$s_select = 'S_SENT_JABBER';
-					}
-					else
-					{
-						trigger_error('FORM_INVALID');
-					}
-				}
-			break;
+				$s_select = 'S_SENT_JABBER';
+			}
+			else
+			{
+				trigger_error('FORM_INVALID');
+			}
 		}
 
 		// Send vars to the template
 		$this->template->assign_vars([
-			'IM_CONTACT'	=> $row[$sql_field],
-			'A_IM_CONTACT'	=> addslashes($row[$sql_field]),
+			'IM_CONTACT'	=> $this->recipient_data['user_jabber'],
+			'A_IM_CONTACT'	=> addslashes($this->recipient_data['user_jabber']),
 
-			'USERNAME'		=> $row['username'],
-			'CONTACT_NAME'	=> $row[$sql_field],
+			'USERNAME'		=> $this->recipient_data['username'],
+			'CONTACT_NAME'	=> $this->recipient_data['user_jabber'],
 			'SITENAME'		=> $this->config['sitename'],
 
-			'PRESENCE_IMG'	=> $presence_img,
-
-			'L_SEND_IM_EXPLAIN'	=> $this->language->lang('IM_' . $lang),
-			'L_IM_SENT_JABBER'	=> $this->language->lang('IM_SENT_JABBER', $row['username']),
+			'L_SEND_IM_EXPLAIN'	=> $this->language->lang('IM_JABBER'),
+			'L_IM_SENT_JABBER'	=> $this->language->lang('IM_SENT_JABBER', $this->recipient_data['username']),
 
 			$s_select		=> true,
 			'S_IM_ACTION'	=> $s_action
 		]);
 
 		return $this->helper->render('memberlist_im.html', $this->language->lang('IM_USER'));
+	}
+
+	/**
+	* Get the recipient's user data
+	*
+	* @param int $user_id Recipient user ID
+	*/
+	protected function get_recipient_data($user_id)
+	{
+		// Grab relevant data
+		$sql = "SELECT user_id, username, user_email, user_lang, user_jabber
+			FROM " . USERS_TABLE . '
+			WHERE user_id = ' . (int) $user_id . '
+				AND ' . $this->db->sql_in_set('user_type', [USER_NORMAL, USER_FOUNDER]);
+		$result = $this->db->sql_query($sql);
+		$this->recipient_data = $this->db->sql_fetchrow($result);
+		$this->db->sql_freeresult($result);
+
+		if ($this->recipient_data === false)
+		{
+			trigger_error('NO_USER');
+		}
+		else if (empty($this->recipient_data['user_jabber']))
+		{
+			trigger_error('IM_NO_DATA');
+		}
+	}
+
+	/**
+	* Send message to the recipient
+	*/
+	protected function send_message()
+	{
+		if (!class_exists('messenger'))
+		{
+			include "{$this->root_path}includes/functions_messenger.{$this->php_ext}";
+		}
+
+		$subject = $this->language->lang('IM_JABBER_SUBJECT', $this->user->data['username'], $this->config['server_name']);
+		$message = $this->request->variable('message', '', true);
+
+		if ($message == '')
+		{
+			trigger_error('EMPTY_MESSAGE_IM');
+		}
+
+		$messenger = new \messenger(false);
+		$messenger->template('profile_send_im', $this->recipient_data['user_lang']);
+		$messenger->subject(htmlspecialchars_decode($subject));
+		$messenger->replyto($this->user->data['user_email']);
+		$messenger->set_addresses($this->recipient_data);
+
+		$messenger->assign_vars([
+			'BOARD_CONTACT'	=> phpbb_get_board_contact($this->config, $this->php_ext),
+			'FROM_USERNAME'	=> htmlspecialchars_decode($this->user->data['username']),
+			'TO_USERNAME'	=> htmlspecialchars_decode($this->recipient_data['username']),
+			'MESSAGE'		=> htmlspecialchars_decode($message)
+		]);
+
+		$messenger->send(NOTIFY_IM);
 	}
 }
